9c5f5a0c7df866d0cb8ecce0aec280d1423dab94
RobertTheDev <robertthedev@gmail.com>
2023-07-11 19:58:18 +0100

    feat: created job post data

---
+++ lib/data/jobPosts.ts
@@ -0,0 +1,13 @@
+import JobPost from 'lib/types/JobPost';
+
+const jobPosts: JobPost[] = [
+  {
+    id: '1',
+    createdAt: new Date('2023-05-21T12:34:56'),
+    updatedAt: null,
+    deadlineDate: new Date('2023-09-21T12:34:56'),
+    title: 'Frontend Developer',
+  },
+];
+
+export default jobPosts;
