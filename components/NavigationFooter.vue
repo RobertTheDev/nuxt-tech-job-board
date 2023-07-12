2d625549dc9704a95651c809cdac76c81409aade
RobertTheDev <robertthedev@gmail.com>
2023-07-12 12:48:36 +0100

    feat: created navigation footer

---
+++ components/NavigationFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="footer-container"><p>footer</p></header>
+</template>
+
+<style>
+.footer-container {
+  align-items: center;
+  background: whitesmoke;
+  display: flex;
+  height: 60px;
+  width: 100%;
+}
+</style>
