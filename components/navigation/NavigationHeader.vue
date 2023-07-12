39bd38f7f36d1ed67aa2b035923c292737dad5cf
RobertTheDev <robertthedev@gmail.com>
2023-07-12 17:57:46 +0100

    feat: created auth buttons

--- components/navigation/NavigationHeader.vue
+++ components/navigation/NavigationHeader.vue
@@ -5,7 +5,8 @@
     <NuxtLink to="/profile/saved-jobs">Saved Jobs</NuxtLink>
     <NuxtLink to="/account-settings/upload-cv">Upload CV</NuxtLink>
     <button @click="toggleProfileMenu()">Profile</button>
-
+    <button @click="navigate('/auth/sign-up')">Sign Up</button>
+    <button @click="navigate('/auth/login')">Login</button>
     <div v-if="profileMenuActive" class="profile-menu-container">
       <NuxtLink to="/job-posts">Browse Jobs</NuxtLink>
       <NuxtLink to="/account-settings">Account Settings</NuxtLink>
@@ -17,10 +18,16 @@
   </header>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import companyName from '../../lib/constants/companyName';
 
 const profileMenuActive = ref(false);
+
+const router = useRouter();
+
+function navigate(href: string) {
+  router.push(href);
+}
 
 function toggleProfileMenu() {
   profileMenuActive.value = !profileMenuActive.value;
