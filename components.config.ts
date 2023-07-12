511ec901e24d27e9e6950ca89c9923429df37b7f
RobertTheDev <robertthedev@gmail.com>
2023-07-12 19:46:21 +0100

    refactor: migrated components configs

---
+++ components.config.ts
@@ -0,0 +1,31 @@
+const componentsConfig = [
+  {
+    path: '~/components',
+  },
+  {
+    path: '~/components/account-settings/',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/auth/',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/companies/',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/job-applications/',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/job-posts/',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/profile/',
+    pathPrefix: false,
+  },
+];
+
+export default componentsConfig;
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,8 @@
+import componentsConfig from './components.config';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['@/styles/index.css'],
-  components: [
-    {
-      path: '~/components',
-    },
-    {
-      path: '~/components/account-settings/',
-      pathPrefix: false,
-    },
-    {
-      path: '~/components/auth/',
-      pathPrefix: false,
-    },
-    {
-      path: '~/components/companies/',
-      pathPrefix: false,
-    },
-    {
-      path: '~/components/job-posts/',
-      pathPrefix: false,
-    },
-    {
-      path: '~/components/profile/',
-      pathPrefix: false,
-    },
-  ],
+  components: componentsConfig,
 });
