6260f6a685a880b18e119087fb713239b52a0878
RobertTheDev <robertthedev@gmail.com>
2023-07-12 14:04:29 +0100

    feat: added footer links to footer

--- components/navigation/NavigationFooter.vue
+++ components/navigation/NavigationFooter.vue
@@ -1,12 +1,27 @@
 <template>
-  <header class="footer-container"><p>footer</p></header>
+  <header class="footer-container">
+    <NuxtLink
+      v-for="footerLink in footerLinks"
+      :key="footerLink.href"
+      :to="footerLink.href"
+      >{{ footerLink.name }}</NuxtLink
+    >
+    <p>© {{ currentYear }} {{ companyName }}</p>
+  </header>
 </template>
+
+<script setup>
+import currentYear from '../../lib/constants/currentYear';
+import companyName from '../../lib/constants/companyName';
+import footerLinks from '../../lib/links/footerLinks';
+</script>
 
 <style>
 .footer-container {
   align-items: center;
   background: whitesmoke;
   display: flex;
+  gap: 10px;
   height: 60px;
   width: 100%;
 }
