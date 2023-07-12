343843da3c35f90682709e9831ba62e60add5526
RobertTheDev <robertthedev@gmail.com>
2023-07-12 11:05:52 +0100

    feat: created basic upload job form

--- pages/job-posts/upload-job.vue
+++ pages/job-posts/upload-job.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
     <p>Upload job</p>
+    <form @submit.prevent="uploadJob()">
+      <label>
+        <input v-model="title" placeholder="title" />
+      </label>
+      <button>Upload</button>
+    </form>
   </div>
 </template>
+
+<script setup>
+const title = ref('');
+
+function uploadJob() {
+  alert(title.value);
+}
+</script>
