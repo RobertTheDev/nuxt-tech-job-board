da27e52ffc685832248719b6b090ee0909b1e46f
RobertTheDev <robertthedev@gmail.com>
2023-07-12 17:47:00 +0100

    feat: created account settings menu

---
+++ components/account-settings/AccountSettingsMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="container">
+      <NuxtLink
+        v-for="accountSettingsMenuLink in accountSettingsMenuLinks"
+        :key="accountSettingsMenuLink.href"
+        :to="accountSettingsMenuLink.href"
+        >{{ accountSettingsMenuLink.name }}</NuxtLink
+      >
+    </div>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import accountSettingsMenuLinks from '../../lib/links/accountSettingsMenuLinks';
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  gap: 10px;
+}
+</style>
