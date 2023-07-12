06ca5e489d521aa5f6427c84a6ca5225666efd2f
RobertTheDev <robertthedev@gmail.com>
2023-07-12 16:58:50 +0100

    feat: added header links

--- components/navigation/NavigationHeader.vue
+++ components/navigation/NavigationHeader.vue
@@ -1,6 +1,16 @@
 <template>
-  <header class="header-container"><p>header</p></header>
+  <header class="header-container">
+    <NuxtLink to="/">{{ companyName }}</NuxtLink>
+    <NuxtLink to="/job-posts/upload-job">Upload job</NuxtLink>
+    <NuxtLink to="/profile/saved-jobs">Saved Jobs</NuxtLink>
+    <NuxtLink to="/account-settings/upload-cv">Upload CV</NuxtLink>
+    <NuxtLink to="/profile">Profile</NuxtLink>
+  </header>
 </template>
+
+<script setup>
+import companyName from '../../lib/constants/companyName';
+</script>
 
 <style>
 .header-container {
@@ -11,5 +21,6 @@
   top: 0px;
   width: 100%;
   background: palegreen;
+  gap: 20px;
 }
 </style>
