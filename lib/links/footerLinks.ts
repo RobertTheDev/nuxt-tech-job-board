4052076390b564f13c135b00ebd60315531342e6
RobertTheDev <robertthedev@gmail.com>
2023-07-12 14:04:17 +0100

    feat: created footer links

---
+++ lib/links/footerLinks.ts
@@ -0,0 +1,24 @@
+const footerLinks = [
+  {
+    href: '/info/about',
+    name: 'About',
+  },
+  {
+    href: '/info/accessibility-statement',
+    name: 'Accessibility Statement',
+  },
+  {
+    href: '/info/cookie-policy',
+    name: 'Cookie Policy',
+  },
+  {
+    href: '/info/privacy-policy',
+    name: 'Privacy Policy',
+  },
+  {
+    href: '/info/terms-and-conditions',
+    name: 'Terms and Conditions',
+  },
+];
+
+export default footerLinks;
