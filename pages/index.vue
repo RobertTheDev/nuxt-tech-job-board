ffb443b938a49122cab3561fdbe31851524ca69f
RobertTheDev <robertthedev@gmail.com>
2023-07-13 13:22:06 +0100

    feat: added seo title and description

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,14 @@
 
 <script setup>
 useHead({
-  title: 'Home',
+  title: 'TechBoard: Job Search',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Welcome to TechBoard - Your Gateway to Tech Remote Jobs. Explore a diverse range of remote job opportunities in the tech industry on our cutting-edge platform. TechBoard is dedicated to connecting talented professionals like you with remote positions in programming, software development, web design, and beyond. Discover the perfect work-from-home job that matches your skills and aspirations. Join our thriving community of remote tech enthusiasts and unlock endless possibilities for growth and success. Embrace the future of work and kickstart your rewarding tech career with TechBoard today..',
+    },
+  ],
 });
 
 const { data: jobPosts } = await useFetch('/api/jobPosts');
