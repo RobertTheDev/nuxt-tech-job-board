ff9dee2c9e0917189fd5524212fb01c01725fd12
RobertTheDev <robertthedev@gmail.com>
2023-07-15 20:01:02 +0100

    feat: created new job posts

--- lib/data/jobPosts.ts
+++ lib/data/jobPosts.ts
@@ -50,55 +50,10 @@ const jobs = [
         'L3Harris Technologies is a Trusted Disruptor for the global aerospace and defense industry. With customers’ mission-critical needs always in mind, our 46,000 employees deliver end-to-end technology solutions connecting the space, air, land, sea and cyber domains.',
       category: 'Defense & Space',
       totalEmployees: 10000,
+      totalJobs: 24,
     },
   },
-  {
-    id: '2',
-    createdAt: new Date('2023-05-21T12:34:56'),
-    updatedAt: null,
-    deadlineDate: new Date('2023-09-21T12:34:56'),
-    totalApplicants: 30,
-    description:
-      'L3Harris Technologies is a Trusted Disruptor for the global aerospace and defense industry. With customers’ mission-critical needs always in mind, our 46,000 employees deliver end-to-end technology solutions connecting the space, air, land, sea and cyber domains.',
-    salary: {
-      currency: 'GBP',
-      min: 70000,
-      max: 90000,
-      rate: 'yearly',
-    },
-    reviews: {
-      totalRating: 15,
-      totalReviews: 4,
-    },
-    title: 'Data Scientist',
-    contractType: 'Full-time',
-    skills: ['Python', 'R', 'Machine Learning'],
-    location: {
-      address: '42 High Street',
-      town: 'London',
-      region: 'Greater London',
-      country: 'United Kingdom',
-    },
-    company: {
-      name: 'UK Home Office',
-      coverImage: {
-        createdAt: new Date('2023-05-21T12:34:56'),
-        updatedAt: new Date('2023-05-21T12:34:56'),
-        url: 'https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-        alt: ' Venture UpCover Image',
-      },
-      logo: {
-        createdAt: new Date('2023-05-21T12:34:56'),
-        updatedAt: new Date('2023-05-21T12:34:56'),
-        url: 'src/assets/companyLogos/Purple Flame.webp',
-        alt: 'UK Home Office Logo',
-      },
-      description:
-        'At the Home Office, we help to ensure that the country is safe and secure. We’ve been looking after UK citizens since 1782.',
-      category: 'Technology',
-      totalEmployees: 500,
-    },
-  },
+
   {
     id: '3',
     createdAt: new Date('2023-05-21T12:34:56'),
@@ -147,6 +102,7 @@ const jobs = [
         Our mission is to become known as the most business outcome focused software delivery partner entirely based in the UK.`,
       category: 'Design',
       totalEmployees: 100,
+      totalJobs: 16,
     },
   },
 
@@ -195,6 +151,7 @@ const jobs = [
         'L3Harris Technologies is a Trusted Disruptor for the global aerospace and defense industry. With customers’ mission-critical needs always in mind, our 46,000 employees deliver end-to-end technology solutions connecting the space, air, land, sea and cyber domains.',
       category: 'Defense & Space',
       totalEmployees: 10000,
+      totalJobs: 5,
     },
   },
   {
@@ -242,6 +199,7 @@ const jobs = [
         'Venture Up is more than just another tech recruiter. By matching exceptional tech talent with ambitious technology ventures, we power your growth. Whether you’re looking to step up your career or build up your tech, design or product team, Venture Up’s expertise in technology recruitment will create the connections you need to grow your business.',
       category: 'Technology',
       totalEmployees: 50,
+      totalJobs: 44,
     },
   },
 ];
