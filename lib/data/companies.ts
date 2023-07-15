5d37cfd7c091d8d4b2f80949819725c7d9f3f940
RobertTheDev <robertthedev@gmail.com>
2023-07-15 01:16:14 +0100

    feat: created companies data

---
+++ lib/data/companies.ts
@@ -0,0 +1,26 @@
+const companiesData = [
+  {
+    id: '3',
+    createdAt: new Date('2023-03-03T12:34:56'),
+    updatedAt: new Date('2023-05-21T12:34:56'),
+    location: null,
+    owner: null,
+    name: 'L3Harris Technologies',
+    coverImage: {
+      createdAt: new Date('2023-05-21T12:34:56'),
+      updatedAt: new Date('2023-05-21T12:34:56'),
+      url: 'https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+      alt: ' Venture UpCover Image',
+    },
+    logo: {
+      createdAt: new Date('2023-05-21T12:34:56'),
+      updatedAt: new Date('2023-05-21T12:34:56'),
+      url: 'https://media.licdn.com/dms/image/C4D0BAQEB2mPDbj_TDA/company-logo_100_100/0/1625156630923?e=1692835200&v=beta&t=KkY0cxvZAFrGBQFj7q8-L0EIl9PfGcpB-C0Z7xOcOCs',
+      alt: 'L3Harris Technologies Logo',
+    },
+    description:
+      'L3Harris Technologies is a Trusted Disruptor for the global aerospace and defense industry. With customers’ mission-critical needs always in mind, our 46,000 employees deliver end-to-end technology solutions connecting the space, air, land, sea and cyber domains.',
+    category: 'Defense & Space',
+    totalEmployees: 10000,
+  },
+];
