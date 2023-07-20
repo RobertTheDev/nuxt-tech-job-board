ce6e464ac5dfc9c7e49bfabb3831e522ac6c8e58
RobertTheDev <robertthedev@gmail.com>
2023-07-20 17:10:58 +0100

    feat: added modal wrapper

--- components/company/CreateCompanyModal.vue
+++ components/company/CreateCompanyModal.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    <button @click="closeCreateCompanyModal">Close</button>
-    <p>Create Company</p>
-    <input placeholder="Name" />
-    <input type="file" placeholder="Logo" />
-    <input type="file" placeholder="Cover Image" />
-    <input placeholder="Description" />
-    <input placeholder="Category" />
-    <input placeholder="Total Employees" />
+  <div class="modal-wrapper">
+    <div class="modal-container">
+      <button @click="closeCreateCompanyModal">Close</button>
+      <p>Create Company</p>
+      <label for="add-company-logo">
+        <input type="file" placeholder="Logo" />
+      </label>
+      <label for="add-company-cover-image">
+        <input type="file" placeholder="Cover Image" />
+      </label>
+      <input placeholder="Name" />
+      <input placeholder="Category" />
+      <input placeholder="Total Employees" />
+      <input placeholder="Description" />
+    </div>
   </div>
 </template>
 
@@ -16,3 +22,27 @@ import useCreateCompanyModalStore from '@/store/createCompanyModalStore';
 
 const { closeCreateCompanyModal } = useCreateCompanyModalStore();
 </script>
+
+<style lang="scss">
+.modal {
+  &-wrapper {
+    min-height: 100vh;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.4);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: fixed;
+    padding: 4.8rem;
+    top: 0px;
+  }
+
+  &-container {
+    width: 400px;
+    background: white;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+  }
+}
+</style>
