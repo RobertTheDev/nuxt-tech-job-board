3be2b350f646bdde0907c92e411da7210ec1b8af
RobertTheDev <robertthedev@gmail.com>
2023-07-20 16:58:07 +0100

    feat: created create company modal

---
+++ components/company/CreateCompanyModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="closeCreateCompanyModal">Close</button>
+    <p>Create Company</p>
+    <input placeholder="Name" />
+    <input type="file" placeholder="Logo" />
+    <input type="file" placeholder="Cover Image" />
+    <input placeholder="Description" />
+    <input placeholder="Category" />
+    <input placeholder="Total Employees" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import useCreateCompanyModalStore from '@/store/createCompanyModalStore';
+
+const { closeCreateCompanyModal } = useCreateCompanyModalStore();
+</script>
