95b97ca5a723e75c512807807dde2e68721ebce3
RobertTheDev <robertthedev@gmail.com>
2023-07-20 20:45:47 +0100

    style: added info page styles

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,3 +1,21 @@
 <template>
-  <ContentDoc />
+  <div class="page-container">
+    <div class="content-container">
+      <ContentDoc />
+    </div>
+  </div>
 </template>
+
+<style>
+.page-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 2rem 0;
+}
+.content-container {
+  display: flex;
+  justify-content: center;
+  width: 50%;
+}
+</style>
