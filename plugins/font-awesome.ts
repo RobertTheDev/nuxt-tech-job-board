915fa5b0dc76c235e54ebf119c941e2ab382945f
RobertTheDev <robertthedev@gmail.com>
2023-07-20 01:06:11 +0100

    feat: added check and cross icons

--- plugins/font-awesome.ts
+++ plugins/font-awesome.ts
@@ -1,14 +1,20 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 // eslint-disable-next-line import/named
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faBookmark } from '@fortawesome/free-regular-svg-icons';
+import {
+  faBookmark,
+  faEye,
+  faEyeSlash,
+  faCircleCheck,
+  faCircleXmark,
+} from '@fortawesome/free-regular-svg-icons';
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faBookmark);
+library.add(faBookmark, faEye, faEyeSlash, faCircleXmark, faCircleCheck);
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon);
