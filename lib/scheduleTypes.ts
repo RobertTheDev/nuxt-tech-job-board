23644bf660c19212238fae3d114eb62535b77b18
RobertTheDev <robertthedev@gmail.com>
2023-07-21 21:44:22 +0100

    feat: created schedule types

---
+++ lib/scheduleTypes.ts
@@ -0,0 +1,60 @@
+const scheduleTypes = [
+  {
+    name: 'Flexitime',
+    value: 'Flexitime',
+  },
+  {
+    name: 'Monday to Friday',
+    value: 'Monday to Friday',
+  },
+  {
+    name: 'Day shift',
+    value: 'Day shift',
+  },
+  {
+    name: 'Night shift',
+    value: 'Night shift',
+  },
+  {
+    name: 'Overtime',
+    value: 'Overtime',
+  },
+  {
+    name: 'Weekend availability',
+    value: 'Weekend availability',
+  },
+  {
+    name: 'No weekends',
+    value: 'No weekends',
+  },
+  {
+    name: 'Every weekend',
+    value: 'Every weekend',
+  },
+  {
+    name: 'Weekends only',
+    value: 'Weekends only',
+  },
+  {
+    name: '8 hour shift',
+    value: '8 hour shift',
+  },
+  {
+    name: '10 hour shift',
+    value: '10 hour shift',
+  },
+  {
+    name: '12 hour shift',
+    value: '12 hour shift',
+  },
+  {
+    name: 'Holidays',
+    value: 'Holidays',
+  },
+  {
+    name: 'Other',
+    value: 'Other',
+  },
+];
+
+export default scheduleTypes;
