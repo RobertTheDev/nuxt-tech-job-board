a0369b3719761a865bf0808f99e6d70bda8a8196
RobertTheDev <robertthedev@gmail.com>
2023-07-21 03:45:53 +0100

    style: updated job post card styles

--- components/job-post/JobPostCard.vue
+++ components/job-post/JobPostCard.vue
@@ -5,38 +5,36 @@
     @click="navigateToJobPost(jobPost.id)"
     @keydown.enter="navigateToJobPost(jobPost.id)"
   >
-    <div class="job-post-card-container">
-      <div class="job-post-card-company-container">
-        <div class="job-post-card-company-logo-container">
-          <img
-            :src="jobPost.company.logo.url"
-            :alt="jobPost.company.logo.alt"
-            class="job-post-card-company-logo-image"
-          />
-        </div>
-        <div class="job-post-card-company-content-container">
-          <p>{{ jobPost.company.name }}</p>
-          <p>
-            Posted
-            {{ differenceInDays(new Date(), new Date(jobPost.createdAt)) }} days
-            ago.
-          </p>
-        </div>
+    <div class="job-post-card-company-container">
+      <div class="job-post-card-company-logo-container">
+        <img
+          :src="jobPost.company.logo.url"
+          :alt="jobPost.company.logo.alt"
+          class="job-post-card-company-logo-image"
+        />
       </div>
-      <div>
-        <font-awesome-icon icon="fa-regular fa-bookmark" />
+      <div class="job-post-card-company-content-container">
+        <p>{{ jobPost.company.name }}</p>
+        <p>
+          Posted
+          {{ differenceInDays(new Date(), new Date(jobPost.createdAt)) }} days
+          ago.
+        </p>
       </div>
-      <p>{{ jobPost.title }}</p>
-      <div class="job-post-card-details-container">
-        <p>{{ jobPost.locationType }}</p>
-        <p>{{ jobPost.contractType }}</p>
-      </div>
-
-      <p>
-        £{{ jobPost.salary.min.toLocaleString() }} -
-        {{ jobPost.salary.max.toLocaleString() }}
-      </p>
     </div>
+    <div>
+      <font-awesome-icon icon="fa-regular fa-bookmark" />
+    </div>
+    <p>{{ jobPost.title }}</p>
+    <div class="job-post-card-details-container">
+      <p>{{ jobPost.locationType }}</p>
+      <p>{{ jobPost.contractType }}</p>
+    </div>
+
+    <p>
+      £{{ jobPost.salary.min.toLocaleString() }} -
+      {{ jobPost.salary.max.toLocaleString() }}
+    </p>
   </div>
 </template>
 
