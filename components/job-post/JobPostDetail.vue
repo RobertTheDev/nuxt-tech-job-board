e2d50a2035a6b97efe8c7b7f79ad058035df0bf3
RobertTheDev <robertthedev@gmail.com>
2023-07-21 04:27:18 +0100

    feat: created job detail content

--- components/job-post/JobPostDetail.vue
+++ components/job-post/JobPostDetail.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div v-if="!jobPost"><p>No Job</p></div>
   <div v-if="jobPost">
     <p>{{ jobPost.title }}</p>
@@ -35,4 +35,59 @@ async function getJobPostById(id: string) {
 }
 
 getJobPostById(id);
+</script> -->
+
+<template>
+  <div class="container">
+    <div class="job-post-detail-company-cover-image-container">
+      <img
+        class="job-post-detail-company-cover-image"
+        :src="jobPost.company.coverImage.url"
+        :alt="jobPost.company.coverImage.alt"
+      />
+    </div>
+    <button><font-awesome-icon icon="fa-regular fa-bookmark" /></button>
+    <p>{{ jobPost.title }}</p>
+    <p>
+      Posted
+      {{ differenceInDays(new Date(), new Date(jobPost.createdAt)) }} days ago.
+    </p>
+    <p>{{ jobPost.totalApplicants }} applicants</p>
+    <p>
+      £{{ jobPost.salary.min.toLocaleString() }} -
+      {{ jobPost.salary.max.toLocaleString() }}
+    </p>
+    <p>{{ jobPost.contractType }}</p>
+    <p>
+      Closes at
+      {{ format(new Date(jobPost.deadlineDate), 'hh:mm:ss, dd MMMM yyyy') }}
+    </p>
+    <p>{{ jobPost.locationType }}</p>
+
+    <p>{{ jobPost.location.address }}</p>
+    <p>{{ jobPost.company.name }}</p>
+    <p>{{ jobPost.description }}</p>
+  </div>
+</template>
+
+<script setup>
+import { differenceInDays, format } from 'date-fns';
+const { data: jobPost } = await useFetch('/api/jobPosts/3');
 </script>
+
+<style lang="scss" scoped>
+.job-post-detail-company-cover-image {
+  width: 80%;
+  height: 100%;
+  object-fit: cover;
+  &-container {
+    width: 100%;
+    height: 240px;
+  }
+}
+.container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
