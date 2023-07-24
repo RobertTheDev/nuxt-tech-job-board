3ff4d8bae8f5b67932b3101df52183200af399bf
RobertTheDev <robertthedev@gmail.com>
2023-07-24 01:19:33 +0100

    feat: created profile menu links data

---
+++ lib/links/profileMenuLinks.ts
@@ -0,0 +1,11 @@
+import NavigationLink from 'lib/types/NavigationLink';
+
+const profileMenuLinks: NavigationLink[] = [
+  { href: '/job-posts', name: 'Browse Jobs' },
+  { href: '/profile/account-settings', name: 'Account Settings' },
+  { href: '/profile/my-companies', name: 'My Companies' },
+  { href: '/profile/followed-companies', name: 'Followed Companies' },
+  { href: '/profile/my-applications', name: 'My Applications' },
+];
+
+export default profileMenuLinks;
