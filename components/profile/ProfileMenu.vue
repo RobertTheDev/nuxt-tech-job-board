a07f50db68256f3733b23f1836dba0e0b5ccebbe
RobertTheDev <robertthedev@gmail.com>
2023-07-24 01:20:09 +0100

    feat: import profile menu links and mapped links

--- components/profile/ProfileMenu.vue
+++ components/profile/ProfileMenu.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="profile-menu-container">
-    <NuxtLink to="/job-posts">Browse Jobs</NuxtLink>
-    <NuxtLink to="/profile/account-settings">Account Settings</NuxtLink>
-    <NuxtLink to="/profile/my-companies">My Companies</NuxtLink>
-    <NuxtLink to="/profile/followed-companies">Followed Companies</NuxtLink>
-    <NuxtLink to="/profile/my-applications">My Applications</NuxtLink>
-    <button>Log Out</button>
+    <NuxtLink
+      v-for="profileMenuLink of profileMenuLinks"
+      :key="profileMenuLink.href"
+      :to="profileMenuLink.href"
+      >{{ profileMenuLink.name }}</NuxtLink
+    >
   </div>
 </template>
+
+<script setup lang="ts">
+import profileMenuLinks from '@/lib/links/profileMenuLinks';
+</script>
