4c0140bed0fb1aa6f1cc3c123a818af156b91c44
RobertTheDev <robertthedev@gmail.com>
2023-07-24 01:20:44 +0100

    feat: created log out button with functionality

--- components/profile/ProfileMenu.vue
+++ components/profile/ProfileMenu.vue
@@ -6,9 +6,18 @@
       :to="profileMenuLink.href"
       >{{ profileMenuLink.name }}</NuxtLink
     >
+
+    <button @click="logout">Log Out</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import profileMenuLinks from '@/lib/links/profileMenuLinks';
+const { reset } = await useSession();
+
+function logout() {
+  reset().then(() => {
+    window.location.reload();
+  });
+}
 </script>
