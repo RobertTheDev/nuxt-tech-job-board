e91b33156c0df45d8a00a4ba23e6a85c124d29e6
RobertTheDev <robertthedev@gmail.com>
2023-07-24 01:22:31 +0100

    feat: defined new mongodb collections

--- server/lib/collections.ts
+++ server/lib/collections.ts
@@ -1,3 +1,11 @@
 import mongoClient from '../db/mongoClient';
 
+export const applicationsCollection = mongoClient.collection('applications');
+
+export const companiesCollection = mongoClient.collection('companies');
+
+export const jobPostsCollection = mongoClient.collection('jobPosts');
+
+export const savedJobsCollection = mongoClient.collection('savedJobs');
+
 export const usersCollection = mongoClient.collection('users');
