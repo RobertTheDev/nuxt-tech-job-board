ead4401da9b26c7fa7ce537e558bb7e2d749af29
RobertTheDev <robertthedev@gmail.com>
2023-08-01 21:54:17 +0100

    feat: created mongo db collection name constants

--- server/lib/collections.ts
+++ server/lib/collections.ts
@@ -4,8 +4,19 @@ export const applicationsCollection = mongoClient.collection('applications');
 
 export const companiesCollection = mongoClient.collection('companies');
 
+export const companyFollowersCollection =
+  mongoClient.collection('company-followers');
+
+export const companyOwnersCollection = mongoClient.collection('companyOwners');
+
+export const jobInterviewsCollection = mongoClient.collection('jobInterviews');
+
+export const jobOffersCollection = mongoClient.collection('jobOffers');
+
 export const jobPostsCollection = mongoClient.collection('jobPosts');
 
-export const savedJobsCollection = mongoClient.collection('savedJobs');
+export const notificationsCollection = mongoClient.collection('notifications');
+
+export const savedJobPostsCollection = mongoClient.collection('savedJobPosts');
 
 export const usersCollection = mongoClient.collection('users');
