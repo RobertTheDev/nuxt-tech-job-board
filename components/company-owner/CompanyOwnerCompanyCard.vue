c70d33d53d6d6a895cda643fba138e7aeb70ada1
RobertTheDev <robertthedev@gmail.com>
2023-08-02 16:54:25 +0100

    feat: created company owner company card component

---
+++ components/company-owner/CompanyOwnerCompanyCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- Content. -->
+  <div>
+    <p>{{ companyOwner.company.name }}</p>
+    <div>
+      <!-- Button navigates to company's edit page. -->
+      <button @click="navigateTo('/')">Edit Company</button>
+      <!-- Button navigates to company's content page. -->
+      <button>View Company</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Import Nuxt useRouter for custom navigation.
+const router = useRouter();
+
+// Define props to be passed into the component.
+const companyOwner = defineProps({
+  company: {
+    name: String,
+    required: true,
+  },
+});
+
+// Create navigate to path using Nuxt router.
+function navigateTo(path: string) {
+  router.push(path);
+}
+</script>
