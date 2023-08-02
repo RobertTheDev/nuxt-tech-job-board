a2b8aa27bf95c0bf665dc202f1a3b6b6b15b87f5
RobertTheDev <robertthedev@gmail.com>
2023-08-02 17:13:03 +0100

    feat: added and mapped owned companies

--- pages/profile/my-companies.vue
+++ pages/profile/my-companies.vue
@@ -1,30 +1,51 @@
 <template>
   <div>
-    <p>My Companies</p>
-    <div v-for="company in companies" :key="company.id">
-      <img :src="company.logo.url" :alt="company.logo.alt" />
-      <p>{{ company.name }}</p>
-      <div>
-        <button>Edit Company</button>
-        <button>View Company</button>
-      </div>
+    <!-- Page Heading. -->
+    <h3>My Companies</h3>
+    <!-- If data is loading show a loading message -->
+    <div v-if="pending">
+      <p>Loading...</p>
     </div>
+    <!-- If error occurs offer an explanation and refrsh button for the user to try again. -->
+    <div v-if="error">
+      <p>
+        Failed to list companies due to an error. Please refresh to try again.
+      </p>
+      <button @click="refresh()">Refresh</button>
+    </div>
+    <!-- Map the data through the CompanyOwnerCompanyCard component. -->
+    <CompanyOwnerCompanyCard
+      v-for="companyOwner of companyOwners"
+      :key="companyOwner._id"
+      v-bind="companyOwner"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+// Import company owner interface for type checking.
+import CompanyOwner from '../../lib/types/CompanyOwner';
+
+// Unauthenticated middleware added to prevent unauthenticated users from accessing this page.
 definePageMeta({
   middleware: 'unauthenticated',
 });
 
-const { data: companies } = await useFetch('/api/companies');
+// Get owned companies by the authenticated user from the server.
+const {
+  data: companyOwners,
+  pending,
+  error,
+  refresh,
+} = await useFetch<CompanyOwner[]>(`/api/company-owners/profile`);
 
+// Add seo title and description for this page.
 useHead({
   title: 'My Companies',
   meta: [
     {
       name: 'description',
-      content: '',
+      content: `Explore the companies you've created on TechBoard. Create new companies and manage existing ones seamlessly. Optimize your hiring process and attract top talent for your job openings.`,
     },
   ],
 });
