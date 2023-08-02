f8182d3a6bdc71b1356041d2a368225ec2d8f200
RobertTheDev <robertthedev@gmail.com>
2023-08-02 16:51:39 +0100

    refactor: upated fields to create job post schema

--- server/validators/jobPosts/createJobPostSchema.ts
+++ server/validators/jobPosts/createJobPostSchema.ts
@@ -2,11 +2,54 @@ import * as yup from 'yup';
 
 const createJobPostSchema = yup
   .object({
-    createdAt: yup.date().default(() => new Date()),
-    title: yup.string().required('A job title is required for a job post.'),
     companyId: yup
       .string()
       .required('A company id is required for a job post.'),
+    createdAt: yup.date().default(() => new Date()),
+    description: yup
+      .string()
+      .required('A job description is required for a job post.'),
+    category: yup
+      .string()
+      .required('A job category is required for a job post.'),
+    contractType: yup
+      .string()
+      .required('Location type is required.')
+      .oneOf(['Contract', 'Full-Time', 'Part-Time'], 'Invalid contract type.'),
+    deadlineDate: yup
+      .date()
+      .required('Deadline date is required.')
+      .min(new Date(), 'Deadline date cannot be in the past or current date.')
+      .max(
+        new Date(new Date().setMonth(new Date().getMonth() + 3)),
+        'Deadline date cannot be more than 3 months in the future.',
+      ),
+    location: yup.string().optional(),
+    locationType: yup
+      .string()
+      .required('Location type is required.')
+      .oneOf(['Remote', 'Hybrid', 'On-Site'], 'Invalid location type.'),
+    totalEmployees: yup.string().required('Total employees is required.'),
+    published: yup.boolean().default(false),
+    salary: yup
+      .object({
+        amount: yup.number().required('Salary amount is required.'),
+        rate: yup
+          .string()
+          .oneOf(
+            ['Hourly', 'Daily', 'Weekly', 'Monthly', 'Annually'],
+            'Invalid rate.',
+          )
+          .default('Annually'),
+      })
+      .required('Salary details are required.'),
+    skills: yup
+      .array()
+      .of(yup.string())
+      .required('Skills are required.')
+      .min(3, 'Minimum of 3 skills are required.')
+      .max(20, 'Maximum of 20 skills are allowed.'),
+    title: yup.string().required('A job title is required for a job post.'),
   })
   .unknown(false);
 
