d3112020352a2b596f36780d424d8bc9599afd98
RobertTheDev <robertthedev@gmail.com>
2023-08-03 22:34:22 +0100

    refactor: added new validation fields

--- server/validators/companies/createCompanySchema.ts
+++ server/validators/companies/createCompanySchema.ts
@@ -4,6 +4,36 @@ const createCompanySchema = yup
   .object({
     createdAt: yup.date().default(() => new Date()),
     name: yup.string().required('A company name is required.'),
+    description: yup.string().required('A company description is required.'),
+    category: yup.string().required('A company category is required.'),
+    totalEmployees: yup
+      .string()
+      .oneOf([
+        '<100',
+        '100-500',
+        '500-1000',
+        '1000-5000',
+        '5000-10000',
+        '10000+',
+      ])
+      .required(),
+    logo: yup
+      .object()
+      .shape({
+        createdAt: yup.date().default(() => new Date()),
+        url: yup.string().url().required('Company logo url is required.'),
+        alt: yup.string().required('Company logo alt is required.'),
+      })
+      .required('A company logo is required.'),
+    coverImage: yup
+      .object()
+      .shape({
+        createdAt: yup.date().default(() => new Date()),
+        url: yup.string().url().required(), // Make url optional
+        alt: yup.string().required(), // Make alt optional
+      })
+      .nullable()
+      .default(null),
   })
   .unknown(false);
 
