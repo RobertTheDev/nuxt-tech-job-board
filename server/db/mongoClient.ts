8c91e9314ac19e713d495631c7c35f5b2cd49098
RobertTheDev <robertthedev@gmail.com>
2023-08-07 22:11:44 +0100

    ci: updated database name

--- server/db/mongoClient.ts
+++ server/db/mongoClient.ts
@@ -5,7 +5,7 @@ const url = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(url);
 
 // // Database Name
-const dbName = 'Restaurant';
+const dbName = 'TechBoard';
 
 const mongoClient = client.db(dbName);
 
