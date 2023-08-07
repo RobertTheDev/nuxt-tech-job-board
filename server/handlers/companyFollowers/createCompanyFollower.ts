91e81ffb336a0f87cdb30d67965d96c6965484c2
RobertTheDev <robertthedev@gmail.com>
2023-08-07 22:17:41 +0100

    ci: removed created at date field

--- server/handlers/companyFollowers/createCompanyFollower.ts
+++ server/handlers/companyFollowers/createCompanyFollower.ts
@@ -1,14 +1,13 @@
 import { ObjectId } from 'mongodb';
-import { companyOwnersCollection } from '../../lib/collections';
 import { CreateCompanyFollowerSchemaType } from '../../validators/companyFollowers/createCompanyFollowerSchema';
+import { companyFollowersCollection } from '../../lib/collections';
 import getCompanyFollowerById from './getCompanyFollowerById';
 
 export default async function createCompanyFollower(
   body: CreateCompanyFollowerSchemaType,
 ) {
   // Create the company follower.
-  const createdCompanyFollower = await companyOwnersCollection.insertOne({
-    createdAt: body.createdAt,
+  const createdCompanyFollower = await companyFollowersCollection.insertOne({
     companyId: new ObjectId(body.companyId),
     userId: new ObjectId(body.userId),
   });
