164652d857b2cb24bf4c21c87b61fb6d09b176d4
RobertTheDev <robertthedev@gmail.com>
2023-08-07 22:10:27 +0100

    feat: added basic upload file setup

--- components/account-settings/UploadCVForm.vue
+++ components/account-settings/UploadCVForm.vue
@@ -2,6 +2,21 @@
   <div>
     <h1>Upload CV</h1>
     <p>You currently have no cv uploaded.</p>
-    <button>Upload CV</button>
+    <p>{{ fileName }}</p>
+    <label for="cv">
+      <input name="cv" type="file" accept="*" @change="uploadFile" />
+    </label>
   </div>
 </template>
+
+<script setup>
+import { ref, computed } from 'vue';
+
+const file = ref(null);
+
+const fileName = computed(() => file.value?.name);
+
+const uploadFile = (event) => {
+  file.value = event.target.files[0];
+};
+</script>
