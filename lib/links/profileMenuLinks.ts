6565660b5abf264152c1c914c72fca5f68951b4f
RobertTheDev <robertthedev@gmail.com>
2023-08-07 22:10:52 +0100

    refactor: removed link

--- lib/links/profileMenuLinks.ts
+++ lib/links/profileMenuLinks.ts
@@ -2,7 +2,6 @@ import NavigationLink from 'lib/types/NavigationLink';
 
 const profileMenuLinks: NavigationLink[] = [
   { href: '/profile', name: 'Profile' },
-  { href: '/job-posts', name: 'Browse Jobs' },
   { href: '/profile/account-settings', name: 'Account Settings' },
   { href: '/profile/my-companies', name: 'My Companies' },
   { href: '/profile/followed-companies', name: 'Followed Companies' },
