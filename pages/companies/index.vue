c161740e829bd7a9d4b5929df4b1d58b6a35d13a
RobertTheDev <robertthedev@gmail.com>
2023-08-07 12:42:36 +0100

    feat: fetched companies data and rendered content

--- pages/companies/index.vue
+++ pages/companies/index.vue
@@ -1,17 +1,72 @@
 <template>
-  <div>
-    <p>Companies</p>
+  <div class="companies-page-container">
+    <!-- Page Title. -->
+    <h1>Companies</h1>
+    <div class="spacer" />
+
+    <!-- Show this when company fetch is loading. -->
+    <div v-if="pending">
+      <p>Companies loading...</p>
+    </div>
+
+    <!-- Show this when company fetch returns an error. -->
+    <div v-if="error">
+      <p>There was an error trying to display companies. Please try again.</p>
+      <button @click="refresh()">Try again.</button>
+    </div>
+
+    <!-- Show this when companies are successfully fetched. -->
+    <div v-if="companies" class="company-card-grid-container">
+      <CompanyCard
+        v-for="company of companies"
+        :key="company._id"
+        v-bind="company"
+      />
+    </div>
+
+    <!-- Show this when no companies are returned from the fetch. -->
+    <div v-else>
+      <p>No companies availables</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+// Import company type.
+import Company from '../../lib/types/Company';
+
+// Use Nuxt Fetch to retrieve the companies data.
+const {
+  data: companies,
+  pending,
+  error,
+  refresh,
+} = await useFetch<Company[]>('/api/companies');
+
+// Companies page SEO title and description.
 useHead({
   title: 'Companies',
   meta: [
     {
       name: 'description',
-      content: '',
+      content:
+        'Discover top tech companies to work for today on TechBoard.  Discover top career opportunities in the industry today and elevate your path to a successful career in tech.',
     },
   ],
 });
 </script>
+
+<style lang="scss" scoped>
+.companies-page-container {
+  padding: 3.2rem;
+}
+
+.spacer {
+  height: 3.2rem;
+}
+.company-card-grid-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 3.2rem;
+}
+</style>
