80d103b25e083a6ffcdc4fde5327a9e6db6bb14a
RobertTheDev <robertthedev@gmail.com>
2023-08-10 04:42:44 +0100

    ci: added new fields

--- lib/types/JobPost.ts
+++ lib/types/JobPost.ts
@@ -1,11 +1,25 @@
 import Company from './Company';
 
+interface Salary {
+  min: number;
+  max: number;
+  rate: string;
+}
+
 export default interface JobPost {
   _id: string;
   createdAt: Date;
   updatedAt?: Date;
   company: Company;
   companyId: string;
+  contractType: string;
   deadlineDate: Date;
+  description: string;
+  location: null;
+  locationType: string;
+  published: boolean;
+  salary: Salary;
+  seniorLevel: string;
   title: string;
+  totalApplicants: number;
 }
