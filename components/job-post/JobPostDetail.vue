723772dc154cdc40aaef1a94e307e4148e01a311
RobertTheDev <robertthedev@gmail.com>
2023-08-12 00:36:08 +0100

    feat: added props fields

--- components/job-post/JobPostDetail.vue
+++ components/job-post/JobPostDetail.vue
@@ -1,47 +1,8 @@
-<!-- <template>
-  <div v-if="!jobPost"><p>No Job</p></div>
-  <div v-if="jobPost">
-    <p>{{ jobPost.title }}</p>
-    <p>
-      Posted
-      {{ differenceInDays(new Date(), new Date(jobPost.createdAt)) }} days ago.
-    </p>
-    <p>Closes {{ format(new Date(jobPost.deadlineDate), 'd MMM yyyy') }}</p>
-    <p>{{ jobPost.title }}</p>
-    <p>{{ jobPost.locationType }}</p>
-    <p>{{ jobPost.contractType }}</p>
-    <p>
-      £{{ jobPost.salary.min.toLocaleString() }} -
-      {{ jobPost.salary.max.toLocaleString() }}
-    </p>
-    <p>{{ jobPost.description }}</p>
-    <img :src="jobPost.company.logo.url" :alt="jobPost.company.logo.alt" />
-    <p>{{ jobPost.company.name }}</p>
-  </div>
-</template>
-
-<script setup lang="ts">
-import { differenceInDays, format } from 'date-fns';
-import JobPost from 'lib/types/JobPost';
-const route = useRoute();
-
-const id = ref(route.query.id).value as string;
-
-const jobPost = ref<JobPost | null>(null);
-
-async function getJobPostById(id: string) {
-  const { data } = await useFetch(`/api/jobPosts/${id}`);
-  jobPost.value = data.value;
-}
-
-getJobPostById(id);
-</script> -->
-
 <template>
-  <p>j ob featil</p>
-  <!-- <div class="container">
+  <div v-if="jobPost" class="container">
     <div class="job-post-detail-company-cover-image-container">
       <img
+        v-if="jobPost.company.coverImage"
         class="job-post-detail-company-cover-image"
         :src="jobPost.company.coverImage.url"
         :alt="jobPost.company.coverImage.alt"
@@ -65,15 +26,23 @@ getJobPostById(id);
     </p>
     <p>{{ jobPost.locationType }}</p>
 
-    <p>{{ jobPost.location.address }}</p>
+    <p v-if="jobPost.location">{{ jobPost.location.address }}</p>
     <p>{{ jobPost.company.name }}</p>
     <p>{{ jobPost.description }}</p>
-  </div> -->
+  </div>
 </template>
 
-<script setup>
-// import { differenceInDays, format } from 'date-fns';
-// const { data: jobPost } = await useFetch('/api/jobPosts/3');
+<script setup lang="ts">
+import { differenceInDays, format } from 'date-fns';
+import JobPost from '@/lib/types/JobPost';
+
+const route = useRoute();
+
+const { id } = route.query;
+
+const { data: jobPost } = await useFetch<JobPost | null>(
+  `/api/job-posts/${id}`,
+);
 </script>
 
 <style lang="scss" scoped>
