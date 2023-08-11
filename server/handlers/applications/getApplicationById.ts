4c943416a16410ab81814e05e528b8263e6f7330
RobertTheDev <robertthedev@gmail.com>
2023-08-11 23:02:32 +0100

    ci: added commenting

--- server/handlers/applications/getApplicationById.ts
+++ server/handlers/applications/getApplicationById.ts
@@ -2,5 +2,6 @@ import { ObjectId } from 'mongodb';
 import { applicationsCollection } from '../../lib/collections';
 
 export default async function getApplicationById(id: string) {
+  // Find and return the application by its id.
   return await applicationsCollection.findOne({ _id: new ObjectId(id) });
 }
