bd2f8b7c8a672eced36802f92a7b8fdd2a58f6fb
RobertTheDev <robertthedev@gmail.com>
2023-08-11 22:59:17 +0100

    ci: deleted files

--- server/handlers/savedJobPosts/deleteSavedJobPostsByCompanyId.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from 'mongodb';
-import { savedJobPostsCollection } from '../../lib/collections';
-
-export default function deleteSavedJobPostsByCompanyId(companyId: string) {
-  // Delete saved job posts by their company id from the database.
-  return savedJobPostsCollection.deleteMany({
-    companyId: new ObjectId(companyId),
-  });
-}
