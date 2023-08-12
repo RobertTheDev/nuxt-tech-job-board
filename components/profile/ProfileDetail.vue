bd6e27829165fb1601d072a3d142215696f28910
RobertTheDev <robertthedev@gmail.com>
2023-08-12 06:07:35 +0100

    feat: created profile detail component

--- components/profile/ProfileDetail.vue
+++ components/profile/ProfileDetail.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <p>Profile Detail</p>
+    <p>{{ user._id }}</p>
+    <p>{{ user.createdAt }}</p>
+    <p>{{ user.emailAddress }}</p>
+    <p>{{ user.firstName }}</p>
+    <p>{{ user.lastName }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+const user = defineProps([
+  '_id',
+  'createdAt',
+  'emailAddress',
+  'firstName',
+  'lastName',
+]);
+</script>
