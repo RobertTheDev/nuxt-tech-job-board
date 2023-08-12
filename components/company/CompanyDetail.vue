c701fb3d1a91e463ec6d7925d53b01ff9f2fbe6f
RobertTheDev <robertthedev@gmail.com>
2023-08-12 06:06:02 +0100

    feat: created company detail component

--- components/company/CompanyDetail.vue
+++ components/company/CompanyDetail.vue
@@ -1,5 +1,49 @@
 <template>
   <div>
-    <p>Company Detail</p>
+    <div v-if="company">
+      <div>
+        <img
+          class="company-page-logo"
+          :src="company.logo.url"
+          :alt="company.logo.alt"
+        />
+      </div>
+      <p>{{ company.name }}</p>
+      <p>{{ company.description }}</p>
+      <p>{{ company.category }}</p>
+      <p>{{ company.totalEmployees }} employees</p>
+      <div v-for="owner of company.owners" :key="owner._id">
+        <p>{{ owner.firstName }}</p>
+        <p>{{ owner.lastName }}</p>
+      </div>
+      <h4>Job Posts</h4>
+
+      <p v-for="jobPost in company.jobPosts" :key="jobPost._id">
+        {{ jobPost.title }}
+      </p>
+    </div>
+    <div v-else>
+      <p>No company</p>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const company = defineProps([
+  'createdAt',
+  'logo',
+  'name',
+  'description',
+  'category',
+  'totalEmployees',
+  'owners',
+  'jobPosts',
+]);
+</script>
+
+<style lang="scss">
+.company-page-logo {
+  height: 10rem;
+  width: 10rem;
+}
+</style>
