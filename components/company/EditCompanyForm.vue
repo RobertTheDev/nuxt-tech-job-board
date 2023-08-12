e27dcaf1f5248257ea478ee0f68e869a7d3b4c72
RobertTheDev <robertthedev@gmail.com>
2023-08-12 06:05:44 +0100

    feat: created edit company form component

--- components/company/EditCompanyForm.vue
+++ components/company/EditCompanyForm.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <p>Edit Company Form</p>
+    <div>
+      <input :placeholder="company.name" />
+      <input :placeholder="company.description" />
+      <input :placeholder="company.logo.url" />
+      <input :placeholder="company.logo.alt" />
+      <input :placeholder="company.totalEmployees" />
+      <input :placeholder="company.category" />
+      <button>Update Company</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const company = defineProps([
+  'logo',
+  'name',
+  'description',
+  'category',
+  'totalEmployees',
+]);
+</script>
