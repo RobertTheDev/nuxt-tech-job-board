4a359f94e8a6cef6bae7c840c1c0e49d88a516d6
RobertTheDev <robertthedev@gmail.com>
2023-08-12 07:56:10 +0100

    feat: created snackbar with functionality

---
+++ components/navigation/SnackBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="snackBar.active" class="container">
+    <p>{{ snackBar.message }}</p>
+  </div>
+</template>
+
+<script setup>
+import snackBarStore from '@/store/useSnackBar';
+
+const { snackBar } = snackBarStore();
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 12rem;
+  width: 20rem;
+  position: fixed;
+  left: calc(50% - 10rem);
+  bottom: 5rem;
+  background: red;
+}
+</style>
--- store/useSnackBar.ts
+++ store/useSnackBar.ts
@@ -1,22 +1,29 @@
 import { defineStore } from 'pinia';
 
 export const useSnackBarStore = defineStore('snackBarStore', () => {
-  const snackBar = ref<{ active: boolean }>({
+  const snackBar = ref<{
+    active: boolean;
+    type: string | null;
+    message: string | null;
+  }>({
     active: false,
+    type: null,
+    message: null,
   });
 
-  function openSnackBar() {
+  function openSnackBar(type: string, message: string) {
     snackBar.value.active = true;
-  }
+    snackBar.value.type = type;
+    snackBar.value.message = message;
 
-  function closeSnackBar() {
-    snackBar.value.active = false;
+    setTimeout(() => {
+      snackBar.value.active = false;
+    }, 2000);
   }
 
   return {
     snackBar,
     openSnackBar,
-    closeSnackBar,
   };
 });
 
