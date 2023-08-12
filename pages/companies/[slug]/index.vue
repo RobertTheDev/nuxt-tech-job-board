cbf3298380c0e341e1ec421fddfcf1ec4cde6cf1
RobertTheDev <robertthedev@gmail.com>
2023-08-12 06:09:09 +0100

    feat: imported company detail component

--- pages/companies/[slug]/index.vue
+++ pages/companies/[slug]/index.vue
@@ -1,31 +1,5 @@
 <template>
-  <div>
-    <div v-if="company">
-      <div>
-        <img
-          class="company-page-logo"
-          :src="company.logo.url"
-          :alt="company.logo.alt"
-        />
-      </div>
-      <p>{{ company.name }}</p>
-      <p>{{ company.description }}</p>
-      <p>{{ company.category }}</p>
-      <p>{{ company.totalEmployees }} employees</p>
-      <div v-for="owner of company.owners" :key="owner._id">
-        <p>{{ owner.firstName }}</p>
-        <p>{{ owner.lastName }}</p>
-      </div>
-      <h4>Job Posts</h4>
-
-      <p v-for="jobPost in company.jobPosts" :key="jobPost._id">
-        {{ jobPost.title }}
-      </p>
-    </div>
-    <div v-else>
-      <p>No company</p>
-    </div>
-  </div>
+  <CompanyDetail v-bind="company" />
 </template>
 
 <script setup lang="ts">
@@ -49,10 +23,3 @@ useHead({
   ],
 });
 </script>
-
-<style lang="scss">
-.company-page-logo {
-  height: 10rem;
-  width: 10rem;
-}
-</style>
