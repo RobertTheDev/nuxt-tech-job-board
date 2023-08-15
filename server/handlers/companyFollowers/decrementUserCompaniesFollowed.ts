a961c5da8640b03ad2e1d0e821717a06cb504fd8
RobertTheDev <robertthedev@gmail.com>
2023-08-15 14:12:31 +0100

    feat: created company follower decrement handler

---
+++ server/handlers/companyFollowers/decrementUserCompaniesFollowed.ts
@@ -0,0 +1,18 @@
+import getUserById from '../users/getUserById';
+import updateUserById from '../users/updateUserById';
+import updateUserSchema from '../../validators/users/updateUserSchema';
+
+// This handler decrements a user's companies followed total and returns the updated user.
+
+export default async function decrementUserCompaniesFollowed(userId: string) {
+  // Get the user by its id.
+  const user = await getUserById(userId);
+
+  // Decrement the users followed companies by 1.
+  const validatedBody = await updateUserSchema.validate({
+    companiesFollowed: user.companiesFollowed - 1,
+  });
+
+  // Return the updated user.
+  return await updateUserById(userId, validatedBody);
+}
