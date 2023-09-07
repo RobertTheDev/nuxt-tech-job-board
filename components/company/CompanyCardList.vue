3f0192621a7d61433c134b81dec3e0a14ea60673
RobertTheDev <robertthedev@gmail.com>
2023-09-07 14:48:07 +0100

    feat: added content

--- components/company/CompanyCardList.vue
+++ components/company/CompanyCardList.vue
@@ -1,9 +1,40 @@
 <template>
-  <div>
-    <p>Company cards list</p>
+  <!-- Show this when company fetch is loading. -->
+  <div v-if="pending">
+    <p>Companies loading...</p>
+  </div>
+
+  <!-- Show this when company fetch returns an error. -->
+  <div v-if="error">
+    <p>There was an error trying to display companies. Please try again.</p>
+    <button @click="refresh()">Try again.</button>
+  </div>
+
+  <!-- Show this when companies are successfully fetched. -->
+  <div v-if="companies" class="company-card-grid-container">
+    <CompanyCard
+      v-for="company of companies"
+      :key="company._id"
+      v-bind="company"
+    />
+  </div>
+
+  <!-- Show this when no companies are returned from the fetch. -->
+  <div v-else>
+    <p>No companies availables</p>
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import Company from 'models/company/types/Company';
 
-<style lang="scss" scoped></style>
+// Import company type.
+
+// Use Nuxt Fetch to retrieve the companies data.
+const {
+  data: companies,
+  pending,
+  error,
+  refresh,
+} = await useFetch<Company[]>('/api/companies');
+</script>
