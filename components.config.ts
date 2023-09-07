11e533803f3540fdc3cf590a9e9d5b700e9daca3
RobertTheDev <robertthedev@gmail.com>
2023-09-07 10:51:35 +0100

    ci: added paths for the new components

--- components.config.ts
+++ components.config.ts
@@ -3,27 +3,55 @@ const componentsConfig = [
     path: '~/components',
   },
   {
-    path: '~/components/account-settings/',
+    path: '~/components/auth',
     pathPrefix: false,
   },
   {
-    path: '~/components/auth/',
+    path: '~/components/common',
     pathPrefix: false,
   },
   {
-    path: '~/components/companies/',
+    path: '~/components/company',
     pathPrefix: false,
   },
   {
-    path: '~/components/job-applications/',
+    path: '~/components/companyFollower',
     pathPrefix: false,
   },
   {
-    path: '~/components/job-post/',
+    path: '~/components/companyOwner',
     pathPrefix: false,
   },
   {
-    path: '~/components/profile/',
+    path: '~/components/jobApplication',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/jobInterview',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/jobOffer',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/jobPost',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/navigation',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/profile',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/settings',
+    pathPrefix: false,
+  },
+  {
+    path: '~/components/user',
     pathPrefix: false,
   },
 ];
