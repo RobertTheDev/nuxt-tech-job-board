64054b7067068639256a557b992063cce68ed941
RobertTheDev <robertthedev@gmail.com>
2023-09-07 14:57:47 +0100

    fix: job post model type import

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,8 @@
 </template>
 
 <script setup lang="ts">
-import JobPost from 'models/jobPost/types/JobPost';
+import companyName from '@/utils/constants/companyName';
+import JobPost from '@/models/jobPost/types/JobPost';
 
 const {
   data: jobPosts,
@@ -32,21 +33,12 @@ const {
 } = await useFetch<JobPost[]>('/api/job-posts');
 
 useHead({
-  title: 'TechBoard - Job Search',
+  title: `Job Search | ${companyName} `,
   meta: [
     {
       name: 'description',
-      content:
-        'Welcome to TechBoard - Your Gateway to Tech Remote Jobs. Explore a diverse range of remote job opportunities in the tech industry on our cutting-edge platform. TechBoard is dedicated to connecting talented professionals like you with remote positions in programming, software development, web design, and beyond. Discover the perfect work-from-home job that matches your skills and aspirations. Join our thriving community of remote tech enthusiasts and unlock endless possibilities for growth and success. Embrace the future of work and kickstart your rewarding tech career with TechBoard today..',
+      content: `Welcome to ${companyName} - an online platform for finding tech jobs. Explore a diverse range of remote job opportunities in the tech industry on our cutting-edge platform. ${companyName} is dedicated to connecting talented professionals like you with remote positions in programming, software development, web design, and beyond. Discover the perfect work-from-home job that matches your skills and aspirations. Join our thriving community of remote tech enthusiasts and unlock endless possibilities for growth and success. Embrace the future of work and kickstart your rewarding tech career with ${companyName} today.`,
     },
   ],
 });
 </script>
-
-<style lang="scss" scoped>
-.container {
-  display: flex;
-  width: 100%;
-  background: whitesmoke;
-}
-</style>
