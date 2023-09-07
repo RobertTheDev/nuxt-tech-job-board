b20c0959c383507bfe90936fe1a072b8e4a4ac72
RobertTheDev <robertthedev@gmail.com>
2023-09-07 17:13:21 +0100

    feat: added prop drilling

--- components/company/CompanyCard.vue
+++ components/company/CompanyCard.vue
@@ -1,7 +1,5 @@
 <template>
-  <!-- Links the component card to the company page using Nuxt Link. -->
   <NuxtLink class="company-card-container" :to="`/companies/${company._id}`">
-    <!-- Card logo. -->
     <div class="company-card-logo-container">
       <img
         :src="company.logo.url"
@@ -9,8 +7,6 @@
         class="company-card-logo-image"
       />
     </div>
-
-    <!-- Card details. -->
     <p>{{ company.name }}</p>
     <p>{{ company.totalEmployees }}</p>
     <p>{{ company.category }}</p>
@@ -20,18 +16,10 @@
 </template>
 
 <script setup lang="ts">
-import CompanyFollower from 'lib/types/CompanyFollower';
+import CompanyFollower from '@/models/companyFollower/types/CompanyFollower';
+import Company from '@/models/company/types/Company';
 
-// Defines the props to be passed into the company card component.
-const company = defineProps([
-  // eslint-disable-next-line vue/prop-name-casing
-  '_id',
-  'logo',
-  'name',
-  'totalEmployees',
-  'category',
-  'description',
-]);
+const company = defineProps<Company>();
 
 async function followCompany(e: MouseEvent, id: string) {
   e.preventDefault();
@@ -40,42 +28,4 @@ async function followCompany(e: MouseEvent, id: string) {
     method: 'POST',
   });
 }
-
-// async function unfollowCompany(e: MouseEvent, id: string) {
-//   e.preventDefault();
-
-//   await useFetch<CompanyFollower[]>(`/api/company-followers/${id}`, {
-//     method: 'DELETE',
-//   }).then(() => {
-//     refresh();
-//   });
-// }
 </script>
-
-<style lang="scss" scoped>
-.company-card {
-  &-container {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    border: 1px solid lightgrey;
-    padding: 1.6rem;
-    border-radius: 1rem;
-  }
-  &-logo {
-    &-container {
-      height: 10rem;
-      width: 10rem;
-      border-radius: 100%;
-      overflow: hidden;
-    }
-    &-image {
-      height: 100%;
-      width: 100%;
-      object-fit: cover;
-    }
-  }
-}
-</style>
