e4f853921d2b6c967af938a33f5bbefbe34a4960
RobertTheDev <robertthedev@gmail.com>
2023-09-07 17:24:07 +0100

    feat: added prop drilling

--- components/companyFollower/CompanyFollowerCard.vue
+++ components/companyFollower/CompanyFollowerCard.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
-    <p>Company Folower Card</p>
-  </div>
+  <NuxtLink
+    class="company-card-container"
+    :to="`/companies/${companyFollower.company._id}`"
+  >
+    <div class="company-card-logo-container">
+      <img
+        :src="companyFollower.company.logo.url"
+        :alt="companyFollower.company.logo.alt"
+        class="company-card-logo-image"
+      />
+    </div>
+    <p>{{ companyFollower.company.name }}</p>
+    <p>{{ companyFollower.company.totalEmployees }}</p>
+    <p>{{ companyFollower.company.category }}</p>
+    <p>{{ companyFollower.company.description }}</p>
+  </NuxtLink>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CompanyFollower from '@/models/companyFollower/types/CompanyFollower';
 
-<style lang="scss" scoped></style>
+const companyFollower = defineProps<CompanyFollower>();
+</script>
