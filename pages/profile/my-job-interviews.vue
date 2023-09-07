577ef3c0964ebb371d0d11e97027152a00e442b0
RobertTheDev <robertthedev@gmail.com>
2023-09-07 15:00:38 +0100

    feat: created page

---
+++ pages/profile/my-job-interviews.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>My Job Interviews</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/profile/my-job-offers.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>My Job Offers</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
