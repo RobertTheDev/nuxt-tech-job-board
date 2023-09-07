edc6db63aeb5882246aaf858d517669a02479b6e
RobertTheDev <robertthedev@gmail.com>
2023-09-07 12:34:04 +0100

    feat: added seo title and description

---
+++ pages/companies/create-company.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="companies-page-container">
+    <h1>Create company</h1>
+    <div class="spacer" />
+    <CreateCompanyForm />
+  </div>
+</template>
+
+<script setup lang="ts">
+import companyName from '@/utils/constants/companyName';
+
+useHead({
+  title: `Start Your Company | ${companyName}`,
+  meta: [
+    {
+      name: 'description',
+      content: `Ready to find candidates for your tech companies? Create your company on ${companyName} today! Our platform makes it easy to find candidates for your company, find top talent, and share your job posts with the world."`,
+    },
+  ],
+});
+</script>
--- pages/companies/index.vue
+++ pages/companies/index.vue
@@ -1,73 +1,21 @@
 <template>
   <div class="companies-page-container">
-    <!-- Page Title. -->
     <h1>Companies</h1>
     <div class="spacer" />
-
-    <!-- Show this when company fetch is loading. -->
-    <div v-if="pending">
-      <p>Companies loading...</p>
-    </div>
-
-    <!-- Show this when company fetch returns an error. -->
-    <div v-if="error">
-      <p>There was an error trying to display companies. Please try again.</p>
-      <button @click="refresh()">Try again.</button>
-    </div>
-
-    <!-- Show this when companies are successfully fetched. -->
-    <div v-if="companies" class="company-card-grid-container">
-      <CompanyCard
-        v-for="company of companies"
-        :key="company._id"
-        v-bind="company"
-      />
-    </div>
-
-    <!-- Show this when no companies are returned from the fetch. -->
-    <div v-else>
-      <p>No companies availables</p>
-    </div>
+    <CompanyCardList />
   </div>
 </template>
 
 <script setup lang="ts">
-import Company from 'models/company/types/Company';
+import companyName from '@/utils/constants/companyName';
 
-// Import company type.
-
-// Use Nuxt Fetch to retrieve the companies data.
-const {
-  data: companies,
-  pending,
-  error,
-  refresh,
-} = await useFetch<Company[]>('/api/companies');
-
-// Companies page SEO title and description.
 useHead({
-  title: 'Companies',
+  title: `Find Companies | ${companyName}`,
   meta: [
     {
       name: 'description',
-      content:
-        'Discover top tech companies to work for today on TechBoard.  Discover top career opportunities in the industry today and elevate your path to a successful career in tech.',
+      content: `Discover top tech companies to work for today on ${companyName}.  Discover top career opportunities in the industry today and elevate your path to a successful career in tech.`,
     },
   ],
 });
 </script>
-
-<style lang="scss" scoped>
-.companies-page-container {
-  padding: 3.2rem;
-}
-
-.spacer {
-  height: 3.2rem;
-}
-.company-card-grid-container {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 3.2rem;
-}
-</style>
