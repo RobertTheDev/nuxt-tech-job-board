795dd895f9fa84e71c4eb480b9f0c98cc83e1454
RobertTheDev <robertthedev@gmail.com>
2023-09-12 21:58:35 +0100

    feat: created candidate page

--- pages/candidates/index.vue
+++ pages/candidates/index.vue
@@ -1,9 +1,85 @@
 <template>
-  <p>Candidates</p>
+  <div class="candidate-page-container">
+    <h1 class="text-3xl bold">Candidates</h1>
+    <div class="candidate-card-list">
+      <div
+        v-for="candidate in candidates"
+        :key="candidate.id"
+        class="candidate-card-container"
+      >
+        <div class="candidate-card-top-container">
+          <div class="candidate-card-avatar-container">
+            <img
+              class="candidate-card-avatar-image"
+              :src="candidate.avatar.url"
+              :alt="candidate.name"
+            />
+          </div>
+        </div>
+        <div class="candidate-card-bottom-container">
+          <div class="candidate-card-content-container">
+            <p class="text-lg bold">{{ candidate.name }}</p>
+            <p class="text-md">{{ candidate.jobTitle }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import companyName from '@/utils/constants/companyName';
+
+const candidates = [
+  {
+    id: '1',
+    avatar: {
+      url: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80',
+    },
+    name: 'Gary Allen',
+    jobTitle: 'Database Engineer',
+  },
+  {
+    id: '2',
+    avatar: {
+      url: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80',
+    },
+    name: 'Gary Jones',
+    jobTitle: 'Software Developer',
+  },
+  {
+    id: '3',
+    avatar: {
+      url: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80',
+    },
+    name: 'Gary Jones',
+    jobTitle: 'Data Scientist',
+  },
+  {
+    id: '4',
+    avatar: {
+      url: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80',
+    },
+    name: 'Gary Jones',
+    jobTitle: 'Backend Developer',
+  },
+  {
+    id: '5',
+    avatar: {
+      url: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80',
+    },
+    name: 'Gary Jones',
+    jobTitle: 'Frontend Developer',
+  },
+  {
+    id: '6',
+    avatar: {
+      url: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80',
+    },
+    name: 'Gary Jones',
+    jobTitle: 'UX Designer',
+  },
+];
 
 useHead({
   title: `Find Candidates | ${companyName}`,
