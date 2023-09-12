6b7689512500f190adaf4c1aca5f78ac5a8608e8
RobertTheDev <robertthedev@gmail.com>
2023-09-12 21:29:33 +0100

    feat: created candidates page

---
+++ pages/candidates/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Candidates</p>
+</template>
+
+<script setup lang="ts">
+import companyName from '@/utils/constants/companyName';
+
+useHead({
+  title: `Find Candidates | ${companyName}`,
+  meta: [
+    {
+      name: 'description',
+      content: `Discover top candidates to work for your company today on ${companyName}.  Discover top talent in the tech industry today and find successful candidates to develop and grow your business.`,
+    },
+  ],
+});
+</script>
