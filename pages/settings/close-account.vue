c7ba83b2f8fd4df1cb16cae3629eee65845babbf
RobertTheDev <robertthedev@gmail.com>
2023-09-13 14:51:54 +0100

    style: added settings page styles

--- pages/settings/close-account.vue
+++ pages/settings/close-account.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="settings-page-container">
-    <CloseAccountForm />
+    <div class="settings-page-container-left">
+      <div class="settings-page-menu-container">
+        <NuxtLink
+          v-for="accountSettingsMenuLink in accountSettingsMenuLinks"
+          :key="accountSettingsMenuLink.href"
+          class="settings-page-menu-link"
+          :to="accountSettingsMenuLink.href"
+          >{{ accountSettingsMenuLink.name }}</NuxtLink
+        >
+      </div>
+    </div>
+    <div class="settings-page-container-center">
+      <CloseAccountForm />
+    </div>
+    <div class="settings-page-container-right" />
   </div>
 </template>
 
 <script setup lang="ts">
 import companyName from '@/utils/constants/companyName';
+import accountSettingsMenuLinks from '@/utils/links/accountSettingsMenuLinks';
 
 // Seo Title And Content.
 useHead({
