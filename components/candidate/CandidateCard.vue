92f1a514367de23eeee92a991489c5a63718fdbd
RobertTheDev <robertthedev@gmail.com>
2023-09-13 21:09:20 +0100

    feat: created candidate card component

---
+++ components/candidate/CandidateCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="candidate-card-container">
+    <div class="candidate-card-top-container">
+      <div class="candidate-card-avatar-container">
+        <img
+          class="candidate-card-avatar-image"
+          :src="candidate.avatar.url"
+          :alt="candidate.name"
+        />
+      </div>
+    </div>
+    <div class="candidate-card-bottom-container">
+      <div class="candidate-card-content-container">
+        <p class="text-lg bold">{{ candidate.name }}</p>
+        <p class="text-md">{{ candidate.jobTitle }}</p>
+        <p class="text-md">4 years experience</p>
+      </div>
+    </div>
+    <button>Offer Job</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Candidate from '@/models/candidate/types/Candidate';
+
+const candidate = defineProps<Candidate>();
+</script>
