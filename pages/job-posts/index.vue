2ca465a2d25c03cd5e6fce7ea7062e269f1177c3
RobertTheDev <robertthedev@gmail.com>
2023-09-13 21:38:16 +0100

    feat: added and mapped job post cards

--- pages/job-posts/index.vue
+++ pages/job-posts/index.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
-    <p>Job Posts</p>
+    <h1>Job Posts</h1>
+    <div>
+      <JobPostCard
+        v-for="jobPost in jobPosts"
+        :key="jobPost._id"
+        v-bind="jobPost"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import JobPost from '@/models/jobPost/types/JobPost';
+
+const {
+  data: jobPosts,
+  // pending,
+  // error,
+  // refresh,
+} = await useFetch<JobPost[]>('/api/job-posts');
+
 useHead({
   title: 'Job Posts',
   meta: [
