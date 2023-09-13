32eb6e029d235f3cc4b021ad5cdeb971613a5b18
RobertTheDev <robertthedev@gmail.com>
2023-09-13 01:39:50 +0100

    feat: created notifications page

---
+++ pages/notifications/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Notifications</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import companyName from '@/utils/constants/companyName';
+
+useHead({
+  title: `Notifications | ${companyName}`,
+  meta: [
+    {
+      name: 'description',
+      content: `Your notifications on ${companyName} will appear here. Notifications will keep you up to date on job offers and application updates.`,
+    },
+  ],
+});
+</script>
