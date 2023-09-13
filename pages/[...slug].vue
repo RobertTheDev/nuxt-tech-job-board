3906fc73c7990ec55fd5bbfa34d17218a310230c
RobertTheDev <robertthedev@gmail.com>
2023-09-13 18:34:43 +0100

    style: created and styled info page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,25 +1,7 @@
 <template>
-  <div class="page-container">
-    <div class="content-container">
+  <div class="info-page-container">
+    <div class="info-page-content-container">
       <ContentDoc />
     </div>
   </div>
 </template>
-
-<style scoped lang="scss">
-.page-container {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  padding: 2rem 0;
-}
-.content-container {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  padding: 0 3.2rem;
-  @media (min-width: 640px) {
-    width: 50%;
-  }
-}
-</style>
