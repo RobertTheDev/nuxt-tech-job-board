69e787893984c8ab0fccf359b70ab00b75cedb82
RobertTheDev <robertthedev@gmail.com>
2023-09-13 20:18:48 +0100

    feat: added input options

--- components/jobPost/JobPostFilterMenu.vue
+++ components/jobPost/JobPostFilterMenu.vue
@@ -4,12 +4,14 @@
     <p class="text-sm bold">Type of Employment</p>
     <div class="job-post-filter-menu-option-group">
       <div
-        v-for="option in options"
-        :key="option"
+        v-for="employmentTypeOption in employmentTypeOptions"
+        :key="employmentTypeOption"
         class="job-post-filter-menu-option-container"
       >
         <input class="job-post-filter-menu-option-checkbox" type="checkbox" />
-        <label class="text-xs bold" for="job1">{{ option }}</label>
+        <label class="text-xs bold" for="job1">{{
+          employmentTypeOption
+        }}</label>
       </div>
     </div>
 
@@ -40,30 +42,7 @@
 </template>
 
 <script setup lang="ts">
-const options = [
-  'Full Time Jobs',
-  'Part Time Jobs',
-  'Remote Jobs',
-  'Internship Jobs',
-  'Contract',
-  'Training Jobs',
-];
-
-const seniorLevelOptions = [
-  'Student Level',
-  'Entry Level',
-  'Mid Level',
-  'Senior Level',
-  'Directors',
-  'VP or Above',
-];
-
-const salaryOptions = [
-  '£500 - £1000',
-  '£1000 - £1500',
-  '£1500 - £2000',
-  '£2000 - £3000',
-  '£3000 - £4000',
-  '£4000 - £5000',
-];
+import salaryOptions from '@/utils/inputOptions/salaryOptions';
+import seniorLevelOptions from '@/utils/inputOptions/seniorLevelOptions';
+import employmentTypeOptions from '@/utils/inputOptions/employmentTypeOptions';
 </script>
