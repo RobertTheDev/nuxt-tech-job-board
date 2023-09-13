5a4600ee0d5d2243e8cebdda605b0f7bbb0debae
RobertTheDev <robertthedev@gmail.com>
2023-09-13 23:28:30 +0100

    feat: added div

--- components/jobApplication/JobApplicationCard.vue
+++ components/jobApplication/JobApplicationCard.vue
@@ -1,6 +1,8 @@
 <template>
-  <p>{{ jobApplication.user.emailAddress }}</p>
-  <p>{{ jobApplication.jobPost.title }}</p>
+  <div>
+    <p>{{ jobApplication.user.emailAddress }}</p>
+    <p>{{ jobApplication.jobPost.title }}</p>
+  </div>
 </template>
 
 <script setup lang="ts">
