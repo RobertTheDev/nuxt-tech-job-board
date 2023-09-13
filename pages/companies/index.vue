7fa0191a20b30b53761264bd14ce5747c0740d3c
RobertTheDev <robertthedev@gmail.com>
2023-09-13 21:38:05 +0100

    refactor: moved company cards to companies page

--- pages/companies/index.vue
+++ pages/companies/index.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="companies-page-container">
     <h1 class="text-3xl bold">Companies</h1>
-    <CompanyCardList />
+
+    <div v-if="pending">
+      <p>Companies loading...</p>
+    </div>
+
+    <div v-if="error">
+      <p>There was an error trying to display companies. Please try again.</p>
+      <button @click="refresh()">Try again.</button>
+    </div>
+
+    <div v-if="companies" class="company-card-list">
+      <CompanyCard
+        v-for="company of companies"
+        :key="company._id"
+        v-bind="company"
+      />
+    </div>
+
+    <div v-else>
+      <p>No companies availables</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import companyName from '@/utils/constants/companyName';
+import Company from '@/models/company/types/Company';
+
+const {
+  data: companies,
+  pending,
+  error,
+  refresh,
+} = await useFetch<Company[]>('/api/companies');
 
 useHead({
   title: `Find Companies | ${companyName}`,
