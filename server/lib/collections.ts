bfaaa2b11c4cb9771781d912f38eed5b652388f2
RobertTheDev <robertthedev@gmail.com>
2023-09-14 18:35:25 +0100

    ci: removed candiate and job offer collections

--- server/lib/collections.ts
+++ server/lib/collections.ts
@@ -1,7 +1,5 @@
 import mongoClient from '../db/mongoClient';
 
-export const candidatesCollection = mongoClient.collection('candidates');
-
 export const companiesCollection = mongoClient.collection('companies');
 
 export const companyFollowersCollection =
@@ -13,8 +11,6 @@ export const jobApplicationsCollection =
   mongoClient.collection('jobApplications');
 
 export const jobInterviewsCollection = mongoClient.collection('jobInterviews');
-
-export const jobOffersCollection = mongoClient.collection('jobOffers');
 
 export const jobPostsCollection = mongoClient.collection('jobPosts');
 
