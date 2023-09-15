4073d6b42a2095542c008e9fa00fdd8f30429714
RobertTheDev <robertthedev@gmail.com>
2023-09-15 01:54:54 +0100

    feat: added fields to sign up

--- components/auth/SignUpForm.vue
+++ components/auth/SignUpForm.vue
@@ -9,17 +9,34 @@
             <input type="email" v-bind="field" placeholder="Email" />
           </label>
         </Field>
-
-        <span>
-          <p>
-            Please enter at least 8 character with number, symbol, small and
-            capital letter.
-          </p>
-        </span>
       </div>
 
       <div>
-        <input type="submit" value="Submit Now" />
+        <Field v-slot="{ field }" name="password" type="password">
+          <label for="password">
+            <input type="password" v-bind="field" placeholder="Password" />
+          </label>
+        </Field>
+      </div>
+
+      <div>
+        <Field v-slot="{ field }" name="firstName" type="text">
+          <label for="firstName">
+            <input type="firstName" v-bind="field" placeholder="First Name" />
+          </label>
+        </Field>
+      </div>
+
+      <div>
+        <Field v-slot="{ field }" name="lastName" type="text">
+          <label for="lastName">
+            <input type="lastName" v-bind="field" placeholder="Last Name" />
+          </label>
+        </Field>
+      </div>
+
+      <div>
+        <button type="submit">Sign Up</button>
       </div>
     </Form>
   </div>
