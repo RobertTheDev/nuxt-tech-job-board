1d0043f63f0d92070615fa10a967e3818b48d02b
RobertTheDev <robertthedev@gmail.com>
2023-09-15 12:52:38 +0100

    refactor: prettier formatted

--- server/handlers/company/deleteCompanyById.ts
+++ server/handlers/company/deleteCompanyById.ts
@@ -3,6 +3,7 @@ import { companiesCollection } from '../../lib/collections';
 
 export default function deleteCompanyById(id: string) {
   // Delete company by its id.
+
   return companiesCollection.findOneAndDelete({
     _id: new ObjectId(id),
   });
