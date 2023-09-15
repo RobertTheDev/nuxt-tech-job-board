dc1dfb48dc9979c61c63c95c9f2f4ebf5cc67577
RobertTheDev <robertthedev@gmail.com>
2023-09-15 22:54:41 +0100

    feat: created setting links component

---
+++ components/settings/SettingLinks.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLink
+    v-for="settingsMenuLink in settingsMenuLinks"
+    :key="settingsMenuLink.href"
+    :to="settingsMenuLink.href"
+  >
+    {{ settingsMenuLink.name }}
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import settingsMenuLinks from '@/utils/links/settingsMenuLinks';
+</script>
