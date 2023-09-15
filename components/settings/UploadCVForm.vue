6a04c5f2d41bce65af1f12f2ed7d243696e29b2f
RobertTheDev <robertthedev@gmail.com>
2023-09-15 22:04:31 +0100

    ci: removed fields

--- components/settings/UploadCVForm.vue
+++ components/settings/UploadCVForm.vue
@@ -1,22 +1,3 @@
 <template>
-  <div>
-    <h1>Upload CV</h1>
-    <p>You currently have no cv uploaded.</p>
-    <p>{{ fileName }}</p>
-    <label for="cv">
-      <input name="cv" type="file" accept="*" @change="uploadFile" />
-    </label>
-  </div>
+  <p>Form</p>
 </template>
-
-<script setup lang="ts">
-import { ref, computed } from 'vue';
-
-const file = ref<any>(null);
-
-const fileName = computed(() => file.value.name);
-
-function uploadFile(event: any): void {
-  file.value = event.target.files[0];
-}
-</script>
