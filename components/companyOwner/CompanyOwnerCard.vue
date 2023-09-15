9b2b02bb53341015f06f9fbf458ca90b95f97656
RobertTheDev <robertthedev@gmail.com>
2023-09-15 01:55:08 +0100

    feat: added styles and props

--- components/companyOwner/CompanyOwnerCard.vue
+++ components/companyOwner/CompanyOwnerCard.vue
@@ -1,10 +1,42 @@
 <template>
-  <p>{{ companyOwner.user.emailAddress }}</p>
-  <p>{{ companyOwner.company.name }}</p>
+  <div class="company-card-container">
+    <div class="company-card-top-container">
+      <div class="company-card-logo-container">
+        <img
+          class="company-card-logo-image"
+          :src="companyOwner.company.logo.url"
+          :alt="`Company logo of ${companyOwner.company.name}`"
+        />
+      </div>
+    </div>
+    <div class="company-card-bottom-container">
+      <div class="company-card-content-container">
+        <p class="text-lg bold">{{ companyOwner.company.name }}</p>
+        <p class="text-md">{{ companyOwner.company.category }}</p>
+        <p class="text-md">
+          {{ companyOwner.company.totalEmployees }} employees
+        </p>
+      </div>
+    </div>
+    <button @click="(e) => deleteCompanyOwnerById(e, companyOwner._id)">
+      Delete company
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import CompanyOwner from '@/models/companyOwner/types/CompanyOwner';
 
 const companyOwner = defineProps<CompanyOwner>();
+
+async function deleteCompanyOwnerById(
+  e: MouseEvent,
+  id: string,
+): Promise<void> {
+  e.preventDefault();
+
+  await useFetch<string>(`/api/company-owners/${id}`, {
+    method: 'DELETE',
+  });
+}
 </script>
