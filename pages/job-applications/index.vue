42b2b3fd7d598781b01cdcb5f9a9c2bb850c0192
RobertTheDev <robertthedev@gmail.com>
2023-09-15 23:57:29 +0100

    feat: added data render and error handling

--- pages/job-applications/index.vue
+++ pages/job-applications/index.vue
@@ -1,13 +1,44 @@
 <template>
   <div>
-    <p>My Applications</p>
+    <h1>My Applications</h1>
+    <div v-if="jobApplications">
+      <JobApplicationCard
+        v-for="jobApplication in jobApplications"
+        :key="jobApplication._id"
+        v-bind="jobApplication"
+      />
+    </div>
+
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+
+    <div v-if="error">
+      <p>There was en error.</p>
+      <button @click="refresh()">Refresh</button>
+    </div>
+
+    <div v-else>
+      <p>You have no job interviews.</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-definePageMeta({
-  middleware: ['unauthenticated'],
+import JobApplication from '@/models/jobApplication/types/JobApplication';
+
+const {
+  data: jobApplications,
+  pending,
+  error,
+  refresh,
+} = await useFetch<JobApplication[]>('/api/job-applications', {
+  method: 'GET',
 });
+
+// definePageMeta({
+//   middleware: ['unauthenticated'],
+// });
 
 useHead({
   title: 'My Applications',
--- pages/job-interviews/index.vue
+++ pages/job-interviews/index.vue
@@ -1,10 +1,39 @@
 <template>
   <div>
-    <h1>My Job Interviews</h1>
+    <h1>My Interviews</h1>
+    <div v-if="jobInterviews">
+      <JobInterviewCard
+        v-for="jobInterview in jobInterviews"
+        :key="jobInterview._id"
+        v-bind="jobInterview"
+      />
+    </div>
+
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+
+    <div v-if="error">
+      <p>There was en error.</p>
+      <button @click="refresh()">Refresh</button>
+    </div>
+
+    <div v-else>
+      <p>You have no job interviews.</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import JobInterview from '@/models/jobInterview/types/JobInterview';
+
+const {
+  data: jobInterviews,
+  pending,
+  error,
+  refresh,
+} = await useFetch<JobInterview[]>('/api/job-interviews', { method: 'GET' });
+
 definePageMeta({
   middleware: ['unauthenticated'],
 });
