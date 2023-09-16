6646ae1c5028e08bcc398184559710e6bd30ae7a
RobertTheDev <robertthedev@gmail.com>
2023-09-17 00:02:01 +0100

    feat: created handlers

---
+++ server/handlers/auth/resetPassword.ts
@@ -0,0 +1,43 @@
+import { usersCollection } from '../../lib/collections';
+import User from '@/models/user/types/User';
+
+export default async function resetPassword({
+  password,
+  resetPasswordToken,
+}: {
+  password: string;
+  resetPasswordToken: string;
+}) {
+  const findUser = (await usersCollection.findOne({
+    resetPasswordToken,
+  })) as User | null;
+
+  if (!findUser) {
+    return `No user with reset token ${resetPasswordToken} was found.`;
+  }
+
+  if (!findUser.resetPasswordTokenExpiryTime) {
+    return 'No reset token expiry time was found.';
+  }
+
+  if (!findUser.resetPasswordToken) {
+    return 'No reset token was found.';
+  }
+
+  // Create an expiry time 10 minutes from now.
+  const currentTimeInMS = new Date().getTime();
+
+  if (findUser.resetPasswordTokenExpiryTime >= currentTimeInMS) {
+    // Update the user by its id with the inputted body.
+    await usersCollection.findOneAndUpdate(
+      { resetPasswordToken },
+      {
+        $set: {
+          password,
+        },
+      },
+    );
+  } else {
+    return 'Your reset token has expired. Please request for a new reset token.';
+  }
+}
---
+++ server/handlers/auth/sendPasswordResetToken.ts
@@ -0,0 +1,63 @@
+import crypto from 'node:crypto';
+import sgMail from '@sendgrid/mail';
+import { usersCollection } from '../../lib/collections';
+
+export default async function sendPasswordResetToken(emailAddress: string) {
+  // Create an expiry time 10 minutes from now.
+  const currentTimeInMS = new Date().getTime();
+  const resetPasswordTokenExpiryTime = new Date(
+    currentTimeInMS + 10 * 60 * 1000,
+  ).getTime();
+
+  const resetPasswordToken = crypto.randomUUID();
+
+  sgMail.setApiKey(String(process.env.SENDGRID_API_KEY));
+  const msg = {
+    to: emailAddress,
+    from: 'robertthedev@gmail.com',
+    subject: 'TechBoard Reset Password',
+    html: `
+    <div>
+      <h1>Forgotten your password?</h1>
+      <p>The button below will take you to our TechBoard password reset page with instructions to reset your password.
+      </p>
+      <p>Click the button below to get started.</p>
+      <a
+        target="_blank"
+        href="http://localhost:3000/reset-password/${resetPasswordToken}"
+        style="
+          background-color: #0075ff;
+          border: none;
+          border-radius: 4px;
+          color: white;
+          cursor: pointer;
+          font-family: Poppins, sans-serif;
+          font-size: 14px;
+          font-weight: 500;
+          margin-top: 8px;
+          padding: 8px 16px;
+          text-decoration: none;
+        "
+      >
+        Reset Password
+      </a>
+    </div>
+    `,
+  };
+
+  await sgMail.send(msg);
+
+  await usersCollection.findOneAndUpdate(
+    {
+      emailAddress,
+    },
+    {
+      $set: {
+        resetPasswordToken,
+        resetPasswordTokenExpiryTime,
+      },
+    },
+  );
+
+  return 'Email sent';
+}
