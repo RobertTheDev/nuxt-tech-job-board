70dd3b8e9f214de29e4fb0be4dad9cfb6e839cf4
RobertTheDev <robertthedev@gmail.com>
2023-09-17 21:31:20 +0100

    feat: created page

---
+++ pages/profile/[email]/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Profile</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Profile',
+  meta: [
+    {
+      name: 'description',
+      content: '',
+    },
+  ],
+});
+</script>
