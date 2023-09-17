619855f35ef7097009bf94f27626c9a579698242
RobertTheDev <robertthedev@gmail.com>
2023-09-17 02:43:37 +0100

    feat: added email verified field

--- models/user/types/User.ts
+++ models/user/types/User.ts
@@ -5,6 +5,7 @@ export default interface User {
   firstName: string;
   lastName: string;
   emailAddress: string;
+  emailVerified?: Date;
   resetPasswordTokenExpiryTime?: number;
   resetPasswordToken?: string;
 }
