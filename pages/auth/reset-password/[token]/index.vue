351250fe96a33da91e21b6784081b09c55bd11c0
RobertTheDev <robertthedev@gmail.com>
2023-09-17 01:15:28 +0100

    feat: created reset password page

---
+++ pages/auth/reset-password/[token]/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ResetPasswordForm />
+  </div>
+</template>
+
+<script setup lang="ts">
+import companyName from '@/utils/constants/companyName';
+
+definePageMeta({
+  middleware: ['authenticated'],
+});
+
+useHead({
+  title: `Reset Password | ${companyName}`,
+  meta: [
+    {
+      name: 'description',
+      content: `Complete the form to reset your ${companyName} password. Your password must be at least eight characters long containing a letter, number and special character.`,
+    },
+  ],
+});
+</script>
