807b3b322d4bd2baca1f738ca6a3a7d89dd19d09
RobertTheDev <robertthedev@gmail.com>
2023-09-17 01:06:07 +0100

    feat: created send password reset form component

---
+++ components/auth/SendPasswordResetTokenForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <Form
+    :validation-schema="sendPasswordResetTokenSchema"
+    @submit="handleSubmit"
+  >
+    <Field v-slot="{ field }" name="emailAddress" type="email">
+      <label for="emailAddress">
+        <p>Email Address</p>
+        <input type="email" v-bind="field" placeholder="Email" />
+      </label>
+    </Field>
+    <ErrorMessage name="emailAddress" />
+    <button type="submit">Reset Password</button>
+  </Form>
+</template>
+
+<script setup lang="ts">
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import sendPasswordResetTokenSchema from '@/models/auth/validators/sendPasswordResetTokenSchema';
+
+function handleSubmit(values: any): void {
+  alert(JSON.stringify(values, null, 2));
+}
+</script>
