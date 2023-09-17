b307beb815a2ba41550c29483d8530a7e2e2c296
RobertTheDev <robertthedev@gmail.com>
2023-09-17 02:49:18 +0100

    feat: added status code to response

--- server/api/auth/logout.post.ts
+++ server/api/auth/logout.post.ts
@@ -8,6 +8,7 @@ export default defineEventHandler((event) => {
   event.context.session.user = null;
 
   return {
+    statusCode: 200,
     statusMessage: 'User successfully logged out.',
   };
 });
