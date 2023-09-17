2e91a7472d1e1e01988963df2403aa12570d965f
RobertTheDev <robertthedev@gmail.com>
2023-09-17 06:38:53 +0100

    ci: mimized argon 2 path imports

--- server/lib/passwordManagement.ts
+++ server/lib/passwordManagement.ts
@@ -1,9 +1,9 @@
-import * as argon2 from 'argon2';
+import { hash, verify } from 'argon2';
 
 export function hashPassword(password: string) {
-  return argon2.hash(password);
+  return hash(password);
 }
 
 export function verifyPassword(password: string, hashedPassword: string) {
-  return argon2.verify(password, hashedPassword);
+  return verify(password, hashedPassword);
 }
