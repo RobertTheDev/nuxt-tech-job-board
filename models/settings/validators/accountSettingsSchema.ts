573e8afb8b703dd817d8f794546c01babbe45688
RobertTheDev <robertthedev@gmail.com>
2023-09-17 05:12:17 +0100

    ci: deleted files

--- models/settings/validators/accountSettingsSchema.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup';
-
-const accountSettingsSchema = Yup.object().shape({
-  wesbite: Yup.string()
-    .optional()
-    .url('Website url must be a valid url format.'),
-});
-
-export default accountSettingsSchema;
