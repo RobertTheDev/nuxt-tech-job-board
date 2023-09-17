0c17b98819539aac43c8290be1af47530a652781
RobertTheDev <robertthedev@gmail.com>
2023-09-17 22:57:48 +0100

    feat: created get notications handler

--- server/handlers/notification/getNotifications.ts
+++ server/handlers/notification/getNotifications.ts
@@ -1,5 +1,18 @@
+import { Document } from 'mongodb';
 import { notificationsCollection } from '../../lib/mongoDBCollections';
+import logger from '../../lib/winstonLogger';
 
-export default function getNotifications() {
-  return notificationsCollection.find().toArray();
+// This handler gets all notifications from MongoDB.
+
+export default async function getNotifications(): Promise<Document[]> {
+  try {
+    // Returns all notifications from MongoDB.
+    return await notificationsCollection.find().toArray();
+  } catch (error) {
+    // Handle the error, log it, and throw an error.
+    logger.error('Error getting notifications:', error);
+    throw new Error(
+      'Could not get the notifications due to an error. Please try again.',
+    );
+  }
 }
