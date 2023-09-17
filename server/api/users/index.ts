3f107a63ec95c34de0caa34dbc81c6354c1227ce
RobertTheDev <robertthedev@gmail.com>
2023-09-18 00:00:19 +0100

    ci: added commenting

--- server/api/users/index.ts
+++ server/api/users/index.ts
@@ -1,20 +1,31 @@
 import deleteUsers from '../../handlers/user/deleteUsers';
 import getUsers from '../../handlers/user/getUsers';
 
+// This route gets and deletes all notications.
+// This route creates a new notification.
+
 export default defineEventHandler((event) => {
+  // Define the request method.
   const { method } = event.node.req;
 
+  // This method finds and returns all the app's users.
   if (method === 'GET') {
     try {
+      // Returns all the app's users.
       return getUsers();
     } catch (error) {
+      // If error occurs return an error.
       return error;
     }
   }
+
+  // This method deletes all the app's users.
   if (method === 'DELETE') {
     try {
+      // Deletes all the app's users.
       return deleteUsers();
     } catch (error) {
+      // If error occurs return an error.
       return error;
     }
   }
