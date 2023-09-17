43d2d76f5d672fa5a37cfd267bf783c5eb0d3b72
RobertTheDev <robertthedev@gmail.com>
2023-09-17 23:05:04 +0100

    feat: defined notifications api routes

--- server/api/notifications/index.ts
+++ server/api/notifications/index.ts
@@ -1,11 +1,20 @@
+import deleteNotifications from '../../handlers/notification/deleteNotifications';
 import getNotifications from '../../handlers/notification/getNotifications';
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler((event) => {
   const { method } = event.node.req;
 
   if (method === 'GET') {
     try {
-      return await getNotifications();
+      return getNotifications();
+    } catch (error) {
+      return error;
+    }
+  }
+
+  if (method === 'DELETE') {
+    try {
+      return deleteNotifications();
     } catch (error) {
       return error;
     }
