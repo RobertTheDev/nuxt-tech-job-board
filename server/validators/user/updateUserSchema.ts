40758309caff5be180d538ef41d589b311938fb0
RobertTheDev <robertthedev@gmail.com>
2023-09-17 06:31:13 +0100

    feat: added fields to update user schema

--- server/validators/user/updateUserSchema.ts
+++ server/validators/user/updateUserSchema.ts
@@ -1,17 +1,30 @@
-import * as yup from 'yup';
+// eslint-disable-next-line import/named
+import { date, string, object, InferType, array } from 'yup';
 
-const updateUserSchema = yup
-  .object({
-    updatedAt: yup.date().default(() => new Date()),
-    firstName: yup.string().optional(),
-    lastName: yup.string().optional(),
-    companiesFollowed: yup.number().optional(),
-    companiesOwned: yup.number().optional(),
-    jobPostsApplied: yup.number().optional(),
-    jobPostsSaved: yup.number().optional(),
-  })
-  .unknown(false);
+const updateUserSchema = object({
+  updatedAt: date().default(() => new Date()),
+  mobilePhoneNumber: string().optional(),
+  landlinePhoneNumber: string().optional(),
+  emailAddress: string().email().optional(),
+  xUrl: string().url().optional(),
+  linkedInUrl: string().url().optional(),
+  websiteUrl: string().optional(),
+  dateOfBirth: date().optional(),
+  cv: string().optional(),
+  address: object()
+    .shape({
+      street: string().optional(),
+      city: string().optional(),
+      state: string().optional(),
+      postalCode: string().optional(),
+      country: string().optional(),
+    })
+    .optional(),
+  lookingFor: array()
+    .of(string().oneOf(['remote', 'on-premise', 'hybrid']))
+    .optional(),
+}).unknown(false);
 
-export type UpdateUserSchemaType = yup.InferType<typeof updateUserSchema>;
+export type UpdateUserSchemaType = InferType<typeof updateUserSchema>;
 
 export default updateUserSchema;
