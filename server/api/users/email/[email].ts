6c186959e894e249a1da176863f2d2ea79896999
RobertTheDev <robertthedev@gmail.com>
2023-09-17 21:31:30 +0100

    feat: created put request

--- server/api/users/email/[email].ts
+++ server/api/users/email/[email].ts
@@ -1,11 +1,27 @@
+import checkUserSignedIn from '../../../handlers/auth/checkUserSignedIn';
 import getUserByEmailAddress from '../../../handlers/user/getUserByEmailAddress';
+import updateUserSchema from '../../../validators/user/updateUserSchema';
+import updateUserByEmailAddress from '../../../handlers/user/updateUserByEmailAddress';
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   const { method } = event.node.req;
-
   const { email } = event.context.params as { email: string };
+  const { user } = event.context.session;
 
   if (method === 'GET') {
     return getUserByEmailAddress(email);
+  }
+  if (method === 'PUT') {
+    const body = await readBody(event);
+
+    checkUserSignedIn(user);
+
+    const validatedBody = await updateUserSchema.validate(body);
+
+    const updatedUser = await updateUserByEmailAddress(email, validatedBody);
+
+    event.context.session.user = updatedUser;
+
+    return updatedUser;
   }
 });
