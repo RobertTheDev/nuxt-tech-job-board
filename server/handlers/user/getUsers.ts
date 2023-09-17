973e54d8e7b099404eba80d45815edd2a42e5e31
RobertTheDev <robertthedev@gmail.com>
2023-09-17 23:01:00 +0100

    feat: created get users handler

--- server/handlers/user/getUsers.ts
+++ server/handlers/user/getUsers.ts
@@ -1,6 +1,18 @@
+import { Document } from 'mongodb';
 import { usersCollection } from '../../lib/mongoDBCollections';
+import logger from '../../lib/winstonLogger';
 
-export default function getUsers() {
-  // Find and returns all users and removes password field from the database.
-  return usersCollection.find().project({ password: 0 }).toArray();
+// This handler gets all users from MongoDB.
+
+export default async function getUsers(): Promise<Document[]> {
+  try {
+    // Find and returns all users from MongoDB and removes password field from the database.
+    return await usersCollection.find().project({ password: 0 }).toArray();
+  } catch (error) {
+    // Handle the error, log it, and throw an error.
+    logger.error('Error getting users:', error);
+    throw new Error(
+      'Could not get the users due to an error. Please try again.',
+    );
+  }
 }
