d1df4538766be7fd10aea1467dbb635ee776f7cc
RobertTheDev <robertthedev@gmail.com>
2023-09-18 22:51:55 +0100

    feat: added color to format

--- server/lib/winstonLogger.ts
+++ server/lib/winstonLogger.ts
@@ -1,15 +1,26 @@
 import { createLogger, transports, format } from 'winston';
 
+const alignColorsAndTime = format.combine(
+  format.colorize({
+    all: true,
+  }),
+  format.label({
+    label: '[LOGGER]',
+  }),
+  format.timestamp({
+    format: 'YY-MM-DD HH:mm:ss',
+  }),
+  format.printf(
+    (info) =>
+      ` ${info.label}  ${info.timestamp}  ${info.level} : ${info.message}`,
+  ),
+);
+
 const logger = createLogger({
+  level: 'debug',
   transports: [
     new transports.Console({
-      format: format.combine(
-        format.colorize(),
-        format.timestamp(),
-        format.printf(
-          (info) => `${info.timestamp} ${info.level}: ${info.message}`,
-        ),
-      ),
+      format: format.combine(format.colorize(), alignColorsAndTime),
     }),
   ],
 });
