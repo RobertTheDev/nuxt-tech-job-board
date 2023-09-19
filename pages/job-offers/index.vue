bc1ade118d19500a2db4bb41257143f32d0314bb
RobertTheDev <robertthedev@gmail.com>
2023-09-19 03:37:26 +0100

    feat: created job offers pages

---
+++ pages/job-offers/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Job Offers</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import companyName from '@/utils/constants/companyName';
+
+useHead({
+  title: `Job Offers | ${companyName}`,
+  meta: [
+    {
+      name: 'description',
+      content: '',
+    },
+  ],
+});
+</script>
