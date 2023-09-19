138f9f19cd781891f0dbffa1401428ae85d2fd04
RobertTheDev <robertthedev@gmail.com>
2023-09-19 01:25:01 +0100

    feat: created job offer validators

---
+++ server/validators/jobOffer/updateJobOfferSchema.ts
@@ -0,0 +1,15 @@
+import { string, object, date } from 'yup';
+
+const updateJobOfferSchema = object({
+  createdAt: date().default(() => new Date()),
+  deadlineDate: date().required(),
+  userId: string().required(),
+  jobPostId: string().required(),
+  companyId: string().required(),
+  message: string().optional(),
+  status: string()
+    .default('IN PROGRESS')
+    .oneOf(['CANCELLED', 'DECLINED', 'EXPIRED', 'IN PROGRESS']),
+}).unknown(false);
+
+export default updateJobOfferSchema;
