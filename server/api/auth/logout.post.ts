3b3027984689b14c71ce874ca2054d7579161a6f
RobertTheDev <robertthedev@gmail.com>
2023-09-20 00:45:01 +0100

    ci: deleted files

--- server/api/auth/logout.post.ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler((event) => {
-  event.context.session.user = null;
-
-  return {
-    statusCode: 200,
-    statusMessage: 'User successfully logged out.',
-  };
-});
--- server/lib/passwordManagement.ts
+++
@@ -1,9 +0,0 @@
-import { hash, verify } from 'argon2';
-
-export function hashPassword(password: string) {
-  return hash(password);
-}
-
-export function verifyPassword(password: string, hashedPassword: string) {
-  return verify(password, hashedPassword);
-}
