77160f86e8c09651b03852e8649c86744b96cca8
RobertTheDev <robertthedev@gmail.com>
2023-09-20 00:48:53 +0100

    ci: added new validation fields

--- models/auth/validators/signUpSchema.ts
+++ models/auth/validators/signUpSchema.ts
@@ -1,30 +1,31 @@
 import { string, date, object } from 'yup';
 
-/* The code defines a schema for signing up a user. It uses the `yup` library to define validation
-rules for each field in the schema. */
+/* The code is defining a schema for validating user sign-up data. The schema is created using the
+`object` function from the `yup` library, which allows you to define a schema for an object with
+specific properties and their validation rules. */
 const signUpSchema = object({
   createdAt: date().default(() => new Date()),
   emailAddress: string()
+    .required('Email address is required.')
     .email('Email address must be a valid email format.')
-    .min(1, 'Email address cannot be empty.')
-    .required('Email address is required.'),
+    .min(1, 'Email address cannot be empty.'),
   emailVerified: string().nullable().default(null),
   firstName: string()
-    .min(1, 'First name cannot be empty.')
-    .required('First name is required.'),
+    .required('First name is required.')
+    .min(1, 'First name cannot be empty.'),
   lastName: string()
-    .min(1, 'Last name cannot be empty.')
-    .required('Last name is required.'),
+    .required('Last name is required.')
+    .min(1, 'Last name cannot be empty.'),
   password: string()
+    .required('Password is required.')
     .min(8, 'Password must be at least 8 characters long')
     .matches(/[A-Z]/, 'Password must contain at least one capital letter')
     .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
     .matches(/\d/, 'Password must contain at least one number')
     .matches(
       /[!@#$%^&*(),.?":{}|<>]/,
-      'Password must contain at least one special character',
-    )
-    .required('Password is required.'),
+      'Password must contain at least one special character (e.g., !@#$%^&*()).',
+    ),
 }).unknown(false);
 
 export default signUpSchema;
