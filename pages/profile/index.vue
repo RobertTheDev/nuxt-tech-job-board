cf432012f14a5f062f2611522a9dfe6e23e4ee3f
RobertTheDev <robertthedev@gmail.com>
2023-09-20 01:28:46 +0100

    feat: added profile details with data fetch

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <p>Profile</p>
-    <p>{{ JSON.stringify(session) }}</p>
-    <div v-if="session?.user">
-      <p>{{ session.user.emailAddress }}</p>
+    <div v-if="pending">
+      <p>Loading...</p>
+    </div>
+    <div v-if="error">
+      <button @click="refresh()">Retry</button>
+    </div>
+    <div v-if="user">
+      <ProfileDetails v-bind="user" />
     </div>
     <div v-else>
       <p>No user</p>
@@ -12,14 +17,24 @@
 </template>
 
 <script setup lang="ts">
-const { session } = await useSession();
+import companyName from '@/utils/constants/companyName';
+import User from '@/models/user/types/User';
+
+const {
+  data: user,
+  pending,
+  error,
+  refresh,
+} = await useFetch<User | null>('/api/profile');
 
 useHead({
-  title: 'Profile',
+  title: `${
+    user.value ? user.value.firstName + ' ' + user.value.lastName : 'Profile'
+  } | ${companyName}`,
   meta: [
     {
       name: 'description',
-      content: '',
+      content: `Welcome to ${companyName} - an online platform for finding tech jobs. Explore a diverse range of remote job opportunities in the tech industry on our cutting-edge platform. ${companyName} is dedicated to connecting talented professionals like you with remote positions in programming, software development, web design, and beyond. Discover the perfect work-from-home job that matches your skills and aspirations. Join our thriving community of remote tech enthusiasts and unlock endless possibilities for growth and success. Embrace the future of work and kickstart your rewarding tech career with ${companyName} today.`,
     },
   ],
 });
