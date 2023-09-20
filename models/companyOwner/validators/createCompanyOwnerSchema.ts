0798024904fad69b2e909500130681daf526a656
RobertTheDev <robertthedev@gmail.com>
2023-09-20 03:36:44 +0100

    ci: deleted unecassary validators

--- models/companyOwner/validators/createCompanyOwnerSchema.ts
+++
@@ -1,9 +0,0 @@
-import { date, string, object } from 'yup';
-
-const createCompanyOwnerSchema = object({
-  createdAt: date().default(() => new Date()),
-  companyId: string().required('A company id is required.'),
-  userId: string().required('A user id is required.'),
-}).unknown(false);
-
-export default createCompanyOwnerSchema;
--- models/companyOwner/validators/updateCompanyOwnerSchema.ts
+++
@@ -1,9 +0,0 @@
-import { date, string, object } from 'yup';
-
-const updateCompanyOwnerSchema = object({
-  updatedAt: date().default(() => new Date()),
-  companyId: string().optional(),
-  userId: string().optional(),
-}).unknown(false);
-
-export default updateCompanyOwnerSchema;
