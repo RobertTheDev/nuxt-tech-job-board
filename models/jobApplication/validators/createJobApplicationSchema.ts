1d945939aa68e8a351e4470256941f790634c034
RobertTheDev <robertthedev@gmail.com>
2023-09-20 04:02:36 +0100

    feat: added cv and message fields

--- models/jobApplication/validators/createJobApplicationSchema.ts
+++ models/jobApplication/validators/createJobApplicationSchema.ts
@@ -2,8 +2,8 @@ import { date, object, string } from 'yup';
 
 const createJobApplicationSchema = object({
   createdAt: date().default(() => new Date()),
-  userId: string().required('A user id is required.'),
-  jobPostId: string().required('A company id is required.'),
+  cv: string().required('Your cv is required.'),
+  message: string().nullable().optional(),
 }).unknown(false);
 
 export default createJobApplicationSchema;
--- models/jobApplication/validators/updateJobApplicationSchema.ts
+++ models/jobApplication/validators/updateJobApplicationSchema.ts
@@ -1,9 +1,9 @@
-import { object, date, string } from 'yup';
+import { date, object, string } from 'yup';
 
 const updateJobApplicationSchema = object({
   updatedAt: date().default(() => new Date()),
-  userId: string().optional(),
-  jobPostId: string().optional(),
+  cv: string().optional(),
+  message: string().optional(),
 }).unknown(false);
 
 export default updateJobApplicationSchema;
